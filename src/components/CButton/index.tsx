d67ea1e88861f346e0bded3666c33d36be55fffd
Francisco Garcia <jjjchico1@gmail.com>
2023-09-12 13:43:21 -0600

    feat: custome button component

--- src/components/CButton/index.tsx
+++ src/components/CButton/index.tsx
@@ -1,9 +1,13 @@
+import './index.scss'
+
 interface CButtonProps {
   children: React.ReactNode
   className?: string
   type?: 'button' | 'submit' | 'reset'
   onClick?: (e: any) => void
   disabled?: boolean
+  uppercase?: boolean
+  widht?: 'full' | 'auto'
 }
 
 export const CButton = ({
@@ -12,6 +16,8 @@ export const CButton = ({
   type,
   onClick,
   disabled,
+  uppercase,
+  widht = 'auto',
 }: CButtonProps) => {
   return (
     <button
@@ -19,6 +25,8 @@ export const CButton = ({
       type={type}
       onClick={onClick}
       disabled={disabled}
+      data-uppercase={uppercase}
+      data-width={widht}
     >
       {children}
     </button>
