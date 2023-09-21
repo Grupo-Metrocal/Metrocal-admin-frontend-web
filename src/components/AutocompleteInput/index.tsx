93729030330763f51d2f1b7aa730b0c5eefe09e5
Francisco Garcia <jjjchico1@gmail.com>
2023-09-21 13:08:14 -0600

    feat: create autocomplete-input component

---
+++ src/components/AutocompleteInput/index.tsx
@@ -0,0 +1,73 @@
+import './index.scss'
+import { useRef } from 'react'
+
+interface IList {
+  id: number
+  name: string
+}
+
+interface IAutocompleteInputProps {
+  onChange: (e: any) => void
+  placeholder?: string
+  list: IList[]
+  className?: string
+  label?: string
+  required?: boolean
+  name: string
+  id?: string
+  requiredLabel?: boolean
+  setItemSelected?: (id: number) => void
+}
+export const AutocompleteInput = ({
+  onChange,
+  requiredLabel,
+  placeholder,
+  list,
+  className,
+  label,
+  required,
+  name,
+  id,
+  setItemSelected,
+}: IAutocompleteInputProps) => {
+  const datalistRef = useRef<HTMLDataListElement>(null)
+
+  return (
+    <div className={`autocomplete-input ${className}`}>
+      <label htmlFor="autocomplete-input">
+        {label && (
+          <span className={requiredLabel ? 'required' : ''}>{label}</span>
+        )}
+        <input
+          list="autocomplete-input-list"
+          placeholder={placeholder}
+          required={required}
+          name={name}
+          id={id}
+          autoComplete="off"
+          onChange={(e) => {
+            const option = datalistRef.current?.querySelector(
+              `option[value="${e.target.value}"]`,
+            )
+
+            setItemSelected && setItemSelected(option ? +option.id : -1)
+
+            onChange(e.target)
+          }}
+        />
+      </label>
+
+      <datalist id="autocomplete-input-list" ref={datalistRef}>
+        {list ? (
+          list.map((item) => (
+            <option key={item.id} value={item.name} id={item.id.toString()}>
+              {item.name}
+            </option>
+          ))
+        ) : (
+          <option value="No hay datos" />
+        )}
+      </datalist>
+    </div>
+  )
+}
