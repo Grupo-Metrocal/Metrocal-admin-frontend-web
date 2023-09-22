739edfc5e1b1da666ea5a2376aca576bc2825d0d
Francisco Garcia <jjjchico1@gmail.com>
2023-09-22 11:45:53 -0600

    fix: use removeEquipment, and update equipment

--- src/app/registerEquipment.tsx
+++ src/app/registerEquipment.tsx
@@ -6,25 +6,23 @@ interface IState {
   type_service: string
   count: number
   model: string
-  measuring_range: string
+  measuring_range: boolean
   calibration_method: string
   additional_remarks: string
 }
 interface IRegisterEquipmentProps {
   handleAddEquipment: () => void
   handleRemoveEquipment: (id: number) => void
-  state: IState[]
+  updateEquipmentValue: (id: number, target: any) => void
+  state: any
 }
 
 export default function RegisterEquipment({
   state,
   handleAddEquipment,
+  handleRemoveEquipment,
+  updateEquipmentValue,
 }: IRegisterEquipmentProps) {
-  const tableBodyRef = useRef<HTMLDivElement>(null)
-
-  console.log('state', state)
-  console.log('table ref', tableBodyRef)
-
   return (
     <div className="register-equipment">
       <h5>
@@ -49,12 +47,15 @@ export default function RegisterEquipment({
               <span>Puntos de calibración y/u observaciones adicionales</span>
             </div>
           </div>
-          <div className="table__body" ref={tableBodyRef}>
-            {state.map((item, index) => {
+          <div className="table__body">
+            {state.map((item: IState, index: number) => {
               return renderTableTr({
                 key: index,
                 id: item.id,
+                deleteEquipment: handleRemoveEquipment,
                 onChange: () => console.log('change'),
+                updateEquipmentValue,
+                state: item,
               })
             })}
 
@@ -79,15 +80,32 @@ interface ITableTrProps {
   id: number
   key: number
   onChange: (e: any) => void
+  deleteEquipment: (id: number) => void
+  updateEquipmentValue: (id: number, target: any) => void
+  state?: IState
 }
 
-const renderTableTr = ({ key, id, onChange }: ITableTrProps) => {
+const renderTableTr = ({
+  key,
+  id,
+  deleteEquipment,
+  updateEquipmentValue,
+  state,
+}: ITableTrProps) => {
   return (
     <div className="table__body__tr" key={key} id={'tr' + id.toString()}>
       <div className="table__body__tr__td">
-        <select name="type_service">
-          <option value="">Calibración</option>
-          <option value="">Verificación</option>
+        <select
+          name="type_service"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          defaultValue={state?.type_service}
+          value={state?.type_service}
+        >
+          <option value="" disabled>
+            Seleccione una opción
+          </option>
+          <option value="Calibración">Calibración</option>
+          <option value="Verificación">Verificación</option>
         </select>
       </div>
       <div className="table__body__tr__td">
@@ -96,26 +114,70 @@ const renderTableTr = ({ key, id, onChange }: ITableTrProps) => {
           placeholder="Nombre del equipo"
           name="name"
           autoComplete="off"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          value={state?.name}
         />
       </div>
       <div className="table__body__tr__td">
-        <input type="number" placeholder="Cantidad" min="1" name="count" />
+        <input
+          type="number"
+          placeholder="0"
+          name="count"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          value={state?.count}
+        />
       </div>
       <div className="table__body__tr__td">
-        <input type="text" placeholder="Modelo" name="model" />
+        <input
+          type="text"
+          placeholder="Modelo"
+          name="model"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          value={state?.model}
+        />
       </div>
       <div className="table__body__tr__td">
-        <input type="checkbox" name="measuring_range" />
+        <input
+          type="checkbox"
+          name="measuring_range"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          checked={state?.measuring_range}
+        />
       </div>
       <div className="table__body__tr__td">
-        <select name="calibration_method">
-          <option value="">Comp. Directa Trazable</option>
+        <select
+          name="calibration_method"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          defaultValue={state?.calibration_method}
+          value={state?.calibration_method}
+        >
+          <option value="" disabled>
+            Seleccione una opción
+          </option>
+          <option value="Comp. Directa Trazable">Comp. Directa Trazable</option>
         </select>
       </div>
       <div className="table__body__tr__td">
-        <select name="additional_remarks">
-          <option value="">vacio</option>
+        <select
+          name="additional_remarks"
+          onChange={(e) => updateEquipmentValue(id, e.target)}
+          defaultValue={state?.additional_remarks}
+          value={state?.additional_remarks}
+        >
+          <option value="" disabled>
+            Seleccione una opción
+          </option>
+          <option value="vacio">vacio</option>
         </select>
+      </div>
+
+      <div className="delete-equipment">
+        <button
+          className="delete-equipment__btn"
+          onClick={() => deleteEquipment(id)}
+        >
+          Eliminar
+        </button>
       </div>
     </div>
   )
