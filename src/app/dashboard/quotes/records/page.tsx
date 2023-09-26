e4dd473aac2d49a6bccc399bdf61ae53bdaf64e1
Francisco Garcia <jjjchico1@gmail.com>
2023-09-26 11:39:55 -0600

    feat: create pages architecture

---
+++ src/app/dashboard/quotes/records/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>records</h1>
+    </div>
+  )
+}
---
+++ src/app/dashboard/reports/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Reports</h1>
+    </div>
+  )
+}
