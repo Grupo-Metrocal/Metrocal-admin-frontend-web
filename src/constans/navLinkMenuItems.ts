f7efc4db1aee389f77ccdb72833fae3a71a21a0e
Francisco Garcia <jjjchico1@gmail.com>
2023-09-26 15:10:24 -0600

    feat: add properties - icon, cetogory

--- src/constans/navLinkMenuItems.ts
+++ src/constans/navLinkMenuItems.ts
@@ -1,13 +1,25 @@
+import activitiesIcon from '../assets/icons/activities.svg'
+import certificatesIcon from '../assets/icons/certificates.svg'
+import dashboardIcon from '../assets/icons/dashboard.svg'
+import quotesIcon from '../assets/icons/quotes.svg'
+import reportsIcon from '../assets/icons/reports.svg'
+import settingsIcon from '../assets/icons/settings.svg'
+import usersIcon from '../assets/icons/users.svg'
+
 export const NAVLINK_MENU_LIST = [
   {
     name: 'Panel de control',
     href: '/dashboard',
     segment: 'dashboard',
+    category: 'organización',
+    icon: dashboardIcon,
   },
   {
     name: 'Cotizaciones ',
     href: '',
     segment: 'quotes',
+    category: 'organización',
+    icon: quotesIcon,
     collection: [
       {
         name: 'Solicitudes',
@@ -23,25 +35,35 @@ export const NAVLINK_MENU_LIST = [
     name: 'certificados',
     href: '/dashboard/certificates',
     segment: 'certificates',
+    category: 'organización',
+    icon: certificatesIcon,
   },
   {
     name: 'Usuarios',
     href: '/dashboard/users',
     segment: 'users',
+    category: 'organización',
+    icon: usersIcon,
   },
   {
     name: 'Actividades',
     href: '/dashboard/activities',
     segment: 'activities',
+    category: 'organización',
+    icon: activitiesIcon,
   },
   {
     name: 'Reportes',
     href: '/dashboard/reports',
     segment: 'reports',
+    category: 'descripción general',
+    icon: reportsIcon,
   },
   {
     name: 'Configuración',
     href: '/dashboard/settings',
     segment: 'settings',
+    category: 'descripción general',
+    icon: settingsIcon,
   },
 ]
