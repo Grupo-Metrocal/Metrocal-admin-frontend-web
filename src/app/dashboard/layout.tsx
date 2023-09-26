1b11b9688d722d9bcbe1246db2280b54343a61fd
Francisco Garcia <jjjchico1@gmail.com>
2023-09-26 15:11:32 -0600

    feat: link redering by category

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,10 @@
-import './index.scss'
+import './layout.scss'
 import { Montserrat } from 'next/font/google'
 import { NAVLINK_MENU_LIST } from '@/constans/navLinkMenuItems'
 import NavLink from './NavLink'
+import Image from 'next/image'
+import metrocalLogo from 'public/metrocal.svg'
+import vectorIcon from '@/assets/icons/vector.svg'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -15,32 +18,78 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <main className="dashboard-container">
           <section className="sidebar">
+            <div className="logo">
+              <Image src={metrocalLogo} alt="Metrocal" />
+            </div>
             <nav>
-              <ul>
-                {NAVLINK_MENU_LIST.map((navLink) => (
-                  <li key={navLink.href}>
-                    <NavLink href={navLink.href} segment={navLink.segment}>
-                      {navLink.name}
-                    </NavLink>
-                    {navLink.collection && (
-                      <ul>
-                        {navLink.collection.map((navLink) => (
-                          <li key={navLink.href}>
-                            <NavLink href={navLink.href}>
-                              {navLink.name}
-                            </NavLink>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
+              {
+                <RenderNavLinkByCategory
+                  category="organización"
+                  list={NAVLINK_MENU_LIST}
+                />
+              }
+              {
+                <RenderNavLinkByCategory
+                  category="descripción general"
+                  list={NAVLINK_MENU_LIST}
+                />
+              }
             </nav>
           </section>
           <div className="content">{children}</div>
         </main>
       </body>
     </html>
+  )
+}
+
+const RenderNavLinkByCategory = ({
+  category,
+  list,
+}: {
+  category: string
+  list: any
+}) => {
+  return (
+    <div className="nav-links">
+      <h4>{category}</h4>
+
+      <ul className="nav-links-list">
+        {list.map(
+          (navLink: any) =>
+            navLink.category === category && (
+              <li
+                key={navLink.href}
+                className={navLink.collection && 'collection'}
+                data-collection-height={
+                  navLink.collection && navLink.collection.length
+                }
+              >
+                <NavLink href={navLink.href} segment={navLink.segment}>
+                  <Image src={navLink.icon} alt={navLink.name} />
+                  {navLink.name}
+
+                  {navLink.collection && (
+                    <Image
+                      src={vectorIcon}
+                      alt="Vector"
+                      className="vector-icon"
+                    />
+                  )}
+                </NavLink>
+                {navLink.collection && (
+                  <ul className="nav-links-list-collection">
+                    {navLink.collection.map((navLink: any) => (
+                      <li key={navLink.href}>
+                        <NavLink href={navLink.href}>{navLink.name}</NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ),
+        )}
+      </ul>
+    </div>
   )
 }
