ad5993cd02121462bfcf8795e31e365648aa577f
Francisco Garcia <jjjchico1@gmail.com>
2023-09-27 15:14:10 -0600

    feat: add rollback function to back page

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -1,15 +1,36 @@
+'use client'
 import './index.scss'
+import Image from 'next/image'
+import RollBackIcon from '@/assets/icons/rollback.svg'
+import { useRouter } from 'next/navigation'
 
 interface LayoutPageProps {
   children: React.ReactNode
   classNme?: string
   title: string
+  rollBack?: boolean
 }
 
-export const LayoutPage = ({ children, classNme, title }: LayoutPageProps) => {
+export const LayoutPage = ({
+  children,
+  classNme,
+  title,
+  rollBack,
+}: LayoutPageProps) => {
+  const router = useRouter()
+
+  const handleRollBack = () => router.back()
+
   return (
     <section className={`layout-page ${classNme}`}>
       <div className="layout-page__header">
+        {rollBack && (
+          <Image
+            src={RollBackIcon}
+            alt="RollBackIcon"
+            onClick={handleRollBack}
+          />
+        )}
         <h1 className="layout-page__title">{title}</h1>
       </div>
       <div className="layout-page__content">{children}</div>
