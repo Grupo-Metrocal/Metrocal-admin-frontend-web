5ba2372a17a2b67089f05fa38253ce02b3d2a301
Francisco Garcia <jjjchico1@gmail.com>
2023-09-28 16:13:15 -0600

    fix: delete prices

--- src/app/dashboard/quotes/requests/[id]/page.tsx
+++ src/app/dashboard/quotes/requests/[id]/page.tsx
@@ -7,11 +7,8 @@ import { CInput } from '@/components/CInput'
 import checkMarkIcon from '@/assets/icons/checkmark.svg'
 import pencilIcon from '@/assets/icons/pencil.svg'
 import xMarkIcon from '@/assets/icons/xmark.svg'
-import dollarIcon from '@/assets/icons/dollar.svg'
-import percentIcon from '@/assets/icons/percent.svg'
 import Image from 'next/image'
-import { Content } from '@/components/Content'
-import { CButton } from '@/components/CButton'
+import { RenderPrices } from './component/RenderPrices'
 
 export interface IEquipmentQuoteRequest {
   id: number
@@ -236,74 +233,5 @@ const RenderEquipmentInfoSelected = ({
         </div>
       </div>
     </div>
-  )
-}
-
-const RenderPrices = ({
-  equipment,
-}: {
-  equipment?: IEquipmentQuoteRequest
-}) => {
-  const [discount, setDiscount] = useState<number>(equipment?.discount || 0)
-  const [price, setPrice] = useState<number>(equipment?.discount || 0)
-  const [total, setTotal] = useState<number>(equipment?.discount || 0)
-
-  console.log(equipment)
-
-  return (
-    <Content title="Precios" colorTitle="purple" className="prices-equipment">
-      <div className="prices">
-        <div className="prices__item">
-          <h4>Descuento</h4>
-          <CInput
-            type="number"
-            value={discount.toString()}
-            onChange={(e) => setDiscount(Number(e.value))}
-            icon={percentIcon}
-          />
-        </div>
-        <div className="prices__item">
-          <h4>Precio unitario</h4>
-          <CInput
-            type="number"
-            value={price.toString()}
-            onChange={(e) => setPrice(Number(e.value))}
-            icon={dollarIcon}
-          />
-        </div>
-        <div className="prices__item">
-          <h4>Equipos</h4>
-          <CInput
-            type="number"
-            value={equipment ? equipment.count.toString() : ''}
-            onChange={(e) => {}}
-            icon={dollarIcon}
-          />
-        </div>
-        <div className="prices__item">
-          <h4>Total</h4>
-          <CInput
-            type="number"
-            value={total.toString()}
-            onChange={(e) => setTotal(Number(e.value))}
-            icon={dollarIcon}
-            dissabled={true}
-          />
-        </div>
-      </div>
-
-      <div className="prices__footer">
-        <CButton
-          style={{
-            background: 'none',
-            color: 'tomato',
-            boxShadow: 'none',
-          }}
-        >
-          Rechazar equipo
-        </CButton>
-        <CButton style={{ boxShadow: 'none' }}>Aprobar equipo</CButton>
-      </div>
-    </Content>
   )
 }
