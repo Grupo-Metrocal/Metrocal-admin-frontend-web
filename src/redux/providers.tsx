079aa10e05ca3910a6f00fb8f4e6c22a9c081a68
Francisco Garcia <jjjchico1@gmail.com>
2023-09-29 13:36:32 -0600

    feat: create provider

---
+++ src/redux/providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
+interface IProps {
+  children: React.ReactNode
+}
+
+export const ReduxProvider = ({ children }: IProps) => {
+  return <Provider store={store}>{children}</Provider>
+}
