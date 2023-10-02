92d8e9a17d0e3a3504ce9850a429081bfec86c07
Francisco Garcia <jjjchico1@gmail.com>
2023-10-02 11:27:01 -0600

    feat: calculate total prices to quote request on global state

--- src/redux/features/quote/quoteSlice.ts
+++ src/redux/features/quote/quoteSlice.ts
@@ -3,12 +3,15 @@ import type {
   IClient,
   IEquipmentQuoteRequest,
 } from '@/app/dashboard/quotes/requests/[id]/page'
-import { parse } from 'path'
 
 const initialState = {
   equipment: [] as IEquipmentQuoteRequest[],
   client: {} as IClient,
   selectedEquipment: {} as IEquipmentQuoteRequest,
+  IVA: 0,
+  discount: 0,
+  total: 0,
+  subtotal: 0,
 }
 
 export const quoteSlice = createSlice({
@@ -26,6 +29,19 @@ export const quoteSlice = createSlice({
     },
     setTotal: (state, action) => {
       state.selectedEquipment.total = action.payload
+    },
+    changeTotalEquipment: (state, action) => {
+      const { id, total } = action.payload
+      const equipment = state.equipment.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            total,
+          }
+        }
+        return item
+      })
+      state.equipment = equipment
     },
     changeStatusSelectedEquipment: (state, action) => {
       const { status } = action.payload
@@ -79,6 +95,18 @@ export const quoteSlice = createSlice({
       const { discount } = action.payload
       state.selectedEquipment.discount = Number(discount)
     },
+    setIVA: (state, action) => {
+      state.IVA = action.payload
+    },
+    setTotalQuote: (state, action) => {
+      state.total = action.payload
+    },
+    setSubtotal: (state, action) => {
+      state.subtotal = action.payload
+    },
+    setDiscountQuote: (state, action) => {
+      state.discount = action.payload
+    },
   },
 })
 
@@ -93,6 +121,11 @@ export const {
   changeDiscountSelectedEquipment,
   changeStatus,
   changeStatusSelectedEquipment,
+  setIVA,
+  setTotalQuote,
+  setSubtotal,
+  setDiscountQuote,
+  changeTotalEquipment,
 } = quoteSlice.actions
 
 export default quoteSlice.reducer
@@ -105,13 +138,18 @@ export const handleStatus = (id: number, status: string) => (dispatch: any) => {
 export const handlePrice = (id: number, target: any) => (dispatch: any) => {
   dispatch(changePrice({ id, price: target.value }))
   dispatch(changePriceSelectedEquipment({ price: target.value }))
+  dispatch(changeTotalEquipment({ id, total: target.value }))
   dispatch(calculateTotal())
+  dispatch(calculateSubtotal())
+  dispatch(calculateTotalQuote())
 }
 
 export const handleDiscount = (id: number, target: any) => (dispatch: any) => {
   dispatch(changeDiscount({ id, discount: target.value }))
   dispatch(changeDiscountSelectedEquipment({ discount: target.value }))
   dispatch(calculateTotal())
+  dispatch(calculateSubtotal())
+  dispatch(calculateTotalQuote())
 }
 
 export const calculateTotal = () => (dispatch: any, getState: any) => {
@@ -123,4 +161,41 @@ export const calculateTotal = () => (dispatch: any, getState: any) => {
   }
   const total = price * count - (price * count * discount) / 100
   dispatch(setTotal(total))
+  dispatch(changeTotalEquipment({ id: selectedEquipment.id, total }))
+}
+export const calculateTotalQuote = () => (dispatch: any, getState: any) => {
+  console.log('calculateTotalQuote')
+  const { subtotal, discount, IVA } = getState().quote
+  const totalQuote = subtotal - (subtotal * discount) / 100
+  const totalIVA = totalQuote * (IVA / 100)
+  dispatch(setTotalQuote((totalQuote + totalIVA).toFixed(2)))
+}
+
+export const calculateSubtotal = () => (dispatch: any, getState: any) => {
+  console.log('calculateSubtotal')
+  const { equipment } = getState().quote
+  let subtotal = 0
+
+  equipment.forEach((item: any) => {
+    subtotal += item.total
+  })
+  console.log('subtotal', equipment)
+
+  dispatch(setSubtotal(subtotal))
+}
+
+export const handleIVA = (target: any) => (dispatch: any) => {
+  dispatch(setIVA(target.value))
+  dispatch(calculateTotalQuote())
+}
+
+export const handleDiscountQuote = (target: any) => (dispatch: any) => {
+  dispatch(setDiscountQuote(target.value))
+  dispatch(calculateTotalQuote())
+}
+
+export const handleDispatchOnLoad = () => (dispatch: any) => {
+  dispatch(calculateTotal())
+  dispatch(calculateSubtotal())
+  dispatch(calculateTotalQuote())
 }
