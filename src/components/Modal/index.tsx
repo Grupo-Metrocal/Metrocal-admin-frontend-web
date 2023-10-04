b42717a5c21626a62e940b02511af8ccfa2b5247
Francisco Garcia <jjjchico1@gmail.com>
2023-10-04 13:48:36 -0600

    feat: add new properties to modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,22 +7,47 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 
-export const Modal = () => {
+const SIZE_DIALOG = {
+  sm: 'sm:max-w-sm',
+  md: 'sm:max-w-md',
+  lg: 'sm:max-w-lg',
+  xl: 'sm:max-w-xl',
+  '2xl': 'sm:max-w-2xl',
+  '3xl': 'sm:max-w-3xl',
+}
+
+interface ModalProps {
+  nameButton: string
+  title?: string
+  description?: string
+  Component?: React.FC
+  buttonStyle?: React.CSSProperties
+  size?: keyof typeof SIZE_DIALOG
+}
+
+export const Modal = ({
+  nameButton,
+  title,
+  Component,
+  buttonStyle,
+  description,
+  size = '3xl',
+}: ModalProps) => {
   return (
     <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
+      <DialogTrigger style={buttonStyle}>{nameButton}</DialogTrigger>
       <DialogContent
         style={{
           backgroundColor: '#fff',
         }}
+        className={`${SIZE_DIALOG[size]}`}
       >
         <DialogHeader>
-          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
+
+        {Component && <Component />}
       </DialogContent>
     </Dialog>
   )
