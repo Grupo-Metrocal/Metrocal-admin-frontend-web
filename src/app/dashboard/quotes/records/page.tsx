8999bdb882996afa08d3f515c47b14013dcae889
Francisco Garcia <jjjchico1@gmail.com>
2023-10-11 15:35:12 -0600

    feat: return content list registered quote

--- src/app/dashboard/quotes/records/page.tsx
+++ src/app/dashboard/quotes/records/page.tsx
@@ -1,8 +1,13 @@
 import { LayoutPage } from '@/components/LayoutPage'
+import { RegisterQuoteList } from './component/RegisterList'
+import { Content } from '@/components/Content'
+
 export default function Page() {
   return (
     <LayoutPage title="Cotizaciones / registros">
-      <span>contenido</span>
+      <Content title="Listado" colorTitle="purple">
+        <RegisterQuoteList />
+      </Content>
     </LayoutPage>
   )
 }
