ea9b28abbd8cbd6930b0546cf9afd7011cfc2623
Francisco Garcia <jjjchico1@gmail.com>
2023-10-12 10:02:29 -0600

    feat: register rowSelection reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quoteReducer from './features/quote/quoteSlice'
+import rowSelectionReducer from './features/data_table/rowSelection'
 
 export const store = configureStore({
   reducer: {
     quote: quoteReducer,
+    rowSelection: rowSelectionReducer,
   },
 })
 
