243e611e40dd943a2e9eaccd66118fcb2fe8f0b2
Francisco Garcia <jjjchico1@gmail.com>
2023-10-16 14:34:56 -0600

    feat: import reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quoteReducer from './features/quote/quoteSlice'
 import rowSelectionReducer from './features/data_table/rowSelection'
+import quoteRequestReducer from './features/quote/quoteRequestSlice'
 
 export const store = configureStore({
   reducer: {
     quote: quoteReducer,
     rowSelection: rowSelectionReducer,
+    quoteRequest: quoteRequestReducer,
   },
 })
 
