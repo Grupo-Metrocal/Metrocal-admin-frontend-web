ff0122f999b1e8a08ab92a221d64550ae1950a67
Francisco Garcia <jjjchico1@gmail.com>
2023-10-19 17:27:48 -0600

    feat: render header from user

--- src/app/dashboard/users/components/listUsers.tsx
+++ src/app/dashboard/users/components/listUsers.tsx
@@ -12,6 +12,9 @@ import { Button } from '@/components/ui/button'
 import { MoreHorizontal } from 'lucide-react'
 import Image from 'next/image'
 import metrocalLogo from 'public/metrocal.svg'
+import usersIcon from '@/assets/icons/users.svg'
+import quotesIcon from '@/assets/icons/quotes.svg'
+import { Modal } from '@/components/Modal'
 
 type Props = {
   users: IUser[]
@@ -19,6 +22,31 @@ type Props = {
 export const ListUsers = ({ users }: Props) => {
   return (
     <div className="users-content ">
+      <header className="users-content-header">
+        <h4>Todos los colaboradores</h4>
+
+        <div className="users-content-header-info">
+          <div className="users">
+            <Image src={usersIcon} alt="users" />
+            <span>{users.length} Miembros</span>
+          </div>
+          <div className="activities">
+            <Image src={quotesIcon} alt="quotes" />
+            <span>0 Actividades</span>
+          </div>
+        </div>
+
+        <div className="users-content-header-actions">
+          <Modal
+            buttonStyle={{
+              boxShadow: 'none',
+              borderRadius: '10px',
+            }}
+            nameButton="Asignar actividad"
+            className="c-button"
+          />
+        </div>
+      </header>
       <div className="users-content-body">
         {users?.map((user) => (
           <ItemListUser key={user.id} user={user} />
@@ -47,8 +75,18 @@ export const ItemListUser = ({ user, onClick }: PropsUser) => {
         <div className="user-info-name">
           <Image src={image} alt="user" width={50} height={50} />
           <div className="user-info-name-text">
-            <span>{user.username}</span>
-            <small>{user.roles ? user.roles?.[0] : 'administrador'}</small>
+            <span
+              className="
+                overflow-hidden
+                whitespace-nowrap
+                overflow-ellipsis
+                w-[150px]
+                "
+              title={user.username}
+            >
+              {user.username}
+            </span>
+            <small>{user.roles[0].name}</small>
           </div>
         </div>
         <div className="actions">
@@ -56,7 +94,16 @@ export const ItemListUser = ({ user, onClick }: PropsUser) => {
         </div>
       </div>
 
-      <div className="user-email">{user.email}</div>
+      <div
+        className="user-email
+      overflow-hidden
+      whitespace-nowrap
+      overflow-ellipsis
+      "
+        title={user.email}
+      >
+        {user.email}
+      </div>
     </div>
   )
 }
