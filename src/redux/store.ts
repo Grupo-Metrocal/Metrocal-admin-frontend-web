e85fe10d67df707756b0a53c79f8e6a45d4e8181
Francisco Garcia <jjjchico1@gmail.com>
2023-10-22 16:13:16 -0600

    feat: include user and role global states

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import quoteReducer from './features/quote/quoteSlice'
 import rowSelectionReducer from './features/data_table/rowSelection'
 import quoteRequestReducer from './features/quote/quoteRequestSlice'
+import usersReducer from './features/user/usersSlice'
+import rolesReducer from './features/user/rolesSlice'
 
 export const store = configureStore({
   reducer: {
     quote: quoteReducer,
     rowSelection: rowSelectionReducer,
     quoteRequest: quoteRequestReducer,
+    users: usersReducer,
+    roles: rolesReducer,
   },
 })
 
