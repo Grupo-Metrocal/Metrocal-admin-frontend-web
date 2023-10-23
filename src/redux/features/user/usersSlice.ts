3927924d88a046c3bf02f1eba7e4652079d0f096
Francisco Garcia <jjjchico1@gmail.com>
2023-10-23 11:18:28 -0600

    feat: add role to user and remove role

--- src/redux/features/user/usersSlice.ts
+++ src/redux/features/user/usersSlice.ts
@@ -16,9 +16,22 @@ export const usersSlice = createSlice({
       const { user } = action.payload
       state.users.push(user)
     },
+    addRole: (state, action) => {
+      const { user, role } = action.payload
+      const index = state.users.findIndex((u) => u.id === user.id)
+      state.users[index].roles.push(role)
+    },
+    removeRole: (state, action) => {
+      const { user, role } = action.payload
+      const index = state.users.findIndex((u) => u.id === user.id)
+      const roleIndex = state.users[index].roles.findIndex(
+        (r: any) => r.id === role.id,
+      )
+      state.users[index].roles.splice(roleIndex, 1)
+    },
   },
 })
 
-export const { setUsers, addUser } = usersSlice.actions
+export const { setUsers, addUser, addRole, removeRole } = usersSlice.actions
 
 export default usersSlice.reducer
