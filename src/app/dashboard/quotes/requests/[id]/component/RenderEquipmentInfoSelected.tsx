b4c2e5d48036defce3860f7b7f48c8571653ac9c
Francisco Garcia <jjjchico1@gmail.com>
2023-11-15 13:11:48 -0600

    feat: change input to autocomplete input component

--- src/app/dashboard/quotes/requests/[id]/component/RenderEquipmentInfoSelected.tsx
+++ src/app/dashboard/quotes/requests/[id]/component/RenderEquipmentInfoSelected.tsx
@@ -2,7 +2,12 @@
 import { useAppDispatch, useAppSelector } from '@/redux/hook'
 import type { IEquipmentQuoteRequest } from '../page'
 import { CInput } from '@/components/CInput'
-import { setComment } from '@/redux/features/quote/quoteSlice'
+import {
+  setCalibrationRethod,
+  setComment,
+  setMeasuringRange,
+} from '@/redux/features/quote/quoteSlice'
+import { AutocompleteInput } from '@/components/AutocompleteInput'
 
 interface IProps {
   equipment?: IEquipmentQuoteRequest
@@ -15,13 +20,89 @@ export const RenderEquipmentInfoSelected = ({ equipment }: IProps) => {
     (state) => state.quote.selectedEquipment,
   )
 
+  const measuringRange = [
+    {
+      id: 1,
+      name: '0 - 100 mm',
+    },
+    {
+      id: 2,
+      name: '0 - 150 mm',
+    },
+    {
+      id: 3,
+      name: '0 - 200 mm',
+    },
+  ]
+
   return (
     <div className="equipment-info-selected">
       <div className="equipment-info-selected__header">
-        <h4>
-          Método de calibración:{' '}
-          <span>{selectedEquipment?.calibration_method} </span>
-        </h4>
+        <div className="">
+          <h4>Método de calibración: </h4>
+          <div className="table__body__tr__td">
+            <select
+              name="calibration_method"
+              onChange={(e) => {
+                dispatch(
+                  setCalibrationRethod({
+                    id: selectedEquipment?.id,
+                    calibration_method: e.target.value,
+                  }),
+                )
+              }}
+              value={selectedEquipment?.calibration_method}
+              className="p-4 rounded-md border border-gray-300"
+            >
+              <option value="" disabled>
+                Seleccione el método
+              </option>
+              <option value="Comp. Directa Trazable">
+                Comp. Directa Trazable
+              </option>
+              <option value="NI-MCIT-B-01 Acreditado">
+                NI-MCIT-B-01 Acreditado
+              </option>
+              <option value="NI-MCIT-D-01 Acreditado">
+                NI-MCIT-D-01 Acreditado
+              </option>
+              <option value="NI-MCIT-D-02 Acreditado">
+                NI-MCIT-D-02 Acreditado
+              </option>
+              <option value="NI-MCIT-FQ-01 Trazable">
+                NI-MCIT-FQ-01 Trazable
+              </option>
+              <option value="NI-MCIT-M-01 Acreditado">
+                NI-MCIT-M-01 Acreditado
+              </option>
+              <option value="NI-MCIT-P-01 Acreditado">
+                NI-MCIT-P-01 Acreditado
+              </option>
+              <option value="NI-MCIT-P-02 Trazable">
+                NI-MCIT-P-02 Trazable
+              </option>
+              <option value="NI-MCIT-T-01 Acreditado">
+                NI-MCIT-T-01 Acreditado
+              </option>
+              <option value="NI-MCIT-T-02 Trazable">
+                NI-MCIT-T-02 Trazable
+              </option>
+              <option value="NI-MCIT-T-03 Acreditado">
+                NI-MCIT-T-03 Acreditado
+              </option>
+              <option value="NI-MCIT-T-04 Trazable">
+                NI-MCIT-T-04 Trazable
+              </option>
+              <option value="NI-MCIT-T-05 Acreditado">
+                NI-MCIT-T-05 Acreditado
+              </option>
+              <option value="NI-MCIT-V-01 Acreditado">
+                NI-MCIT-V-01 Acreditado
+              </option>
+              <option value="No Aplica (N/A)">No Aplica (N/A)</option>
+            </select>
+          </div>
+        </div>
 
         <h4>
           Modelo: <span>{selectedEquipment?.model}</span>
@@ -37,14 +118,31 @@ export const RenderEquipmentInfoSelected = ({ equipment }: IProps) => {
           />
         </div>
         <div>
-          <CInput
+          <AutocompleteInput
+            setItemSelected={(item: any) => {
+              const selected = measuringRange.find((e) => e.id === item) || {
+                name: '',
+              }
+              dispatch(
+                setMeasuringRange({
+                  id: selectedEquipment?.id,
+                  measuring_range: selected.name,
+                }),
+              )
+            }}
+            onChange={(e) => {
+              dispatch(
+                setMeasuringRange({
+                  id: selectedEquipment?.id,
+                  measuring_range: e.value,
+                }),
+              )
+            }}
+            list={measuringRange}
+            name="measuring_range"
+            value={selectedEquipment?.measuring_range}
+            placeholder="(0 - 100) mm"
             label="Rango de medición"
-            value={''}
-            onChange={() => {}}
-            dissabled={
-              selectedEquipment?.measuring_range &&
-              selectedEquipment?.measuring_range
-            }
           />
         </div>
 
@@ -58,7 +156,6 @@ export const RenderEquipmentInfoSelected = ({ equipment }: IProps) => {
             }
             name="comment"
             onChange={(e) => {
-              console.log(e.value)
               dispatch(
                 setComment({ id: selectedEquipment?.id, comment: e.value }),
               )
