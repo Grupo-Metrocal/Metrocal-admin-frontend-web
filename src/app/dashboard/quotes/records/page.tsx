e501f35f0d80f505121aac3747228c2e04fecc63
Francisco Garcia <jjjchico1@gmail.com>
2023-11-23 17:07:48 -0600

    feat: render graphics and recent activites component

--- src/app/dashboard/quotes/records/page.tsx
+++ src/app/dashboard/quotes/records/page.tsx
@@ -1,13 +1,23 @@
+import './index.scss'
 import { LayoutPage } from '@/components/LayoutPage'
 import { RegisterQuoteList } from './component/RegisterList'
 import { Content } from '@/components/Content'
+import { RecentQuotes } from './component/RecentQuotes'
+import { RecentActivities } from './component/RecentActivities'
 
 export default function Page() {
   return (
-    <LayoutPage title="Cotizaciones / registros">
-      <Content title="Listado" colorTitle="purple">
-        <RegisterQuoteList />
-      </Content>
+    <LayoutPage title="Cotizaciones / registros" className="quotes-records">
+      <div className="quotes-records">
+        <div className="quotes-records__recent">
+          <RecentActivities />
+          <RecentQuotes />
+        </div>
+
+        <Content title="Listado" colorTitle="purple">
+          <RegisterQuoteList />
+        </Content>
+      </div>
     </LayoutPage>
   )
 }
