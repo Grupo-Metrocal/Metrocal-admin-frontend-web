4156665e38248b8e120137a782002a5ad27accd6
Francisco Garcia <jjjchico1@gmail.com>
2023-11-30 15:05:34 -0600

    feat: Add activities page with data fetching and rendering

--- src/app/dashboard/activities/page.tsx
+++ src/app/dashboard/activities/page.tsx
@@ -1,8 +1,66 @@
+'use client'
+import './index.scss'
 import { LayoutPage } from '@/components/LayoutPage'
+import type { IActivity, ITeammember } from '@/types/activities'
+import { useState, useEffect } from 'react'
+import { fetchData } from '@/utils/fetch'
+import { getCookie } from 'cookies-next'
+import { Spinner } from '@/components/Spinner'
+import { Content } from '@/components/Content'
+import { ActivityItem } from './components/ActivityItem'
+
+const getData = async () => {
+  const response = await fetchData({
+    url: 'activities',
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+    },
+  })
+
+  return response
+}
 export default function Page() {
+  const [activities, setActivities] = useState<IActivity[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    getData().then((response) => {
+      if (response.success) {
+        setActivities(response.data)
+      } else {
+        setActivities([])
+      }
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <LayoutPage title="Actividades">
-      <span>contenido</span>
+      <Content title="Asignación de actividades">
+        <div className="activities-page">
+          <header className="activities-header">
+            <h2>Actividades disponibles: {activities.length}</h2>
+          </header>
+          <div className="activities_content">
+            {loading ? (
+              <div className="flex justify-center items-center">
+                {' '}
+                <Spinner />
+              </div>
+            ) : activities.length > 0 ? (
+              activities.map((activity: IActivity) => (
+                <ActivityItem key={activity.id} activity={activity} />
+              ))
+            ) : (
+              <div className="flex justify-center items-center">
+                {' '}
+                No hay actividades
+              </div>
+            )}
+          </div>
+        </div>
+      </Content>
     </LayoutPage>
   )
 }
