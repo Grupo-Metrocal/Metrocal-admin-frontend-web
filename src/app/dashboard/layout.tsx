1c1f0491ba9a5ff7c17e999e5737ba923e583200
Francisco Garcia <jjjchico1@gmail.com>
2024-01-31 11:33:22 -0600

    feat: Add Modal and Logout functionality

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,6 +16,9 @@ import { Profile } from './ComponentLayout/Profile'
 import { QuoteRequest } from './ComponentLayout/quoteRequest'
 import menuLinesIcon from '@/assets/icons/menu-lines.svg'
 import { useState } from 'react'
+import { Modal } from '@/components/Modal'
+import { Logout } from './ComponentLayout/Logout'
+import logoutIcon from '@/assets/icons/logout.svg'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -31,97 +34,127 @@ export default function RootLayout({
   }
 
   return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        <main className="dashboard-container">
-          <section className={`sidebar ${openMenu && 'open-sidebar'}`}>
-            <div className="logo">
-              <h3>Metrocal</h3>
+    <ReduxProvider>
+      <html lang="en">
+        <body className={montserrat.className}>
+          <main className="dashboard-container">
+            <section className={`sidebar ${openMenu && 'open-sidebar'}`}>
+              <div className="logo">
+                <h3>Metrocal</h3>
 
-              {/* close menu */}
-              <div className="close_menu" onClick={handleOpenMenu}>
-                <Image src={menuLinesIcon} alt="Menu" width={24} height={24} />
+                {/* close menu */}
+                <div className="close_menu" onClick={handleOpenMenu}>
+                  <Image
+                    src={menuLinesIcon}
+                    alt="Menu"
+                    width={24}
+                    height={24}
+                  />
+                </div>
               </div>
-            </div>
-            <nav>
-              {
-                <RenderNavLinkByCategory
-                  category="organización"
-                  list={NAVLINK_MENU_LIST}
-                />
-              }
-              {
-                <RenderNavLinkByCategory
-                  category="descripción general"
-                  list={NAVLINK_MENU_LIST}
-                />
-              }
-            </nav>
-          </section>
-          <div className="content">
-            <header className="header">
-              <div className="open_menu" onClick={handleOpenMenu}>
-                <Image src={menuLinesIcon} alt="Menu" width={24} height={24} />
-              </div>
+              <nav>
+                {
+                  <RenderNavLinkByCategory
+                    category="organización"
+                    list={NAVLINK_MENU_LIST}
+                  />
+                }
+                {
+                  <RenderNavLinkByCategory
+                    category="descripción general"
+                    list={NAVLINK_MENU_LIST}
+                  />
+                }
 
-              <div className="hello">
-                <h4>
-                  Bienvenid@ <span>{getCookie('username')}</span>
-                </h4>
-              </div>
+                {/* cerrar sesion */}
+                <div className="nav-links sessions">
+                  <h4>sesión</h4>
 
-              <div className="user">
-                <div className="quote">
-                  <CSheet
-                    position="top"
-                    styles={{
-                      width: 'fit-content',
-                      margin: '0 auto',
+                  <Modal
+                    title="Estas seguro de cerrar sesión?"
+                    nameButton="Cerrar sesión"
+                    size="sm"
+                    Component={() => <Logout />}
+                    icon={logoutIcon}
+                    marginIcon="1.5em"
+                    buttonStyle={{
+                      padding: '0.5em 1.5em',
+                      width: '100%',
+                      textAlign: 'left',
+                      fontWeight: 'bold',
                     }}
-                    title="Solicitar cotización"
-                    description="Escribe el correo electronico o nombre de la empresa del cliente para solicitar una cotización."
-                    Component={<QuoteRequest />}
-                  >
-                    <CButton
-                      style={{
-                        boxShadow: 'none',
-                      }}
-                      className="quote-button"
-                    >
-                      <span>+</span>{' '}
-                      <span className="quote-text">Solicitar cotización</span>
-                    </CButton>
-                  </CSheet>
+                  />
                 </div>
-                <div className="notification" data-badge="+9">
-                  <Image src={notificationIcon} alt="Notification" />
-                </div>
-                <div className="profile">
-                  <CSheet
-                    title="Edita tu perfil"
-                    description="Haz cambios en tu perfil aquí. Haz clic en guardar cuando hayas terminado."
-                    Component={<Profile />}
-                  >
-                    <div className="flex gap-2 justify-center items-center">
-                      <Image src={metrocalLogo} alt="Profile" />
-                      <span className="name-user font-bold">
-                        {getCookie('username')?.split(' ')[0]}
-                      </span>
-                    </div>
-                  </CSheet>
-                </div>
-              </div>
-            </header>
-            <section className="content-container">
-              <ReduxProvider>{children}</ReduxProvider>
+              </nav>
             </section>
-          </div>
-        </main>
+            <div className="content">
+              <header className="header">
+                <div className="open_menu" onClick={handleOpenMenu}>
+                  <Image
+                    src={menuLinesIcon}
+                    alt="Menu"
+                    width={24}
+                    height={24}
+                  />
+                </div>
 
-        {/* richColor for colored toast*/}
-        <Toaster expand={true} richColors={true} />
-      </body>
-    </html>
+                <div className="hello">
+                  <h4>
+                    Bienvenid@ <span>{getCookie('username')}</span>
+                  </h4>
+                </div>
+
+                <div className="user">
+                  <div className="quote">
+                    <CSheet
+                      position="top"
+                      styles={{
+                        width: 'fit-content',
+                        margin: '0 auto',
+                      }}
+                      title="Solicitar cotización"
+                      description="Escribe el correo electronico o nombre de la empresa del cliente para solicitar una cotización."
+                      Component={<QuoteRequest />}
+                    >
+                      <CButton
+                        style={{
+                          boxShadow: 'none',
+                        }}
+                        className="quote-button"
+                      >
+                        <span>+</span>{' '}
+                        <span className="quote-text">Solicitar cotización</span>
+                      </CButton>
+                    </CSheet>
+                  </div>
+                  <div className="notification" data-badge="+9">
+                    <Image src={notificationIcon} alt="Notification" />
+                  </div>
+                  <div className="profile">
+                    <CSheet
+                      title="Edita tu perfil"
+                      description="Haz cambios en tu perfil aquí. Haz clic en guardar cuando hayas terminado."
+                      Component={<Profile />}
+                    >
+                      <div className="flex gap-2 justify-center items-center">
+                        <Image src={metrocalLogo} alt="Profile" />
+                        <span className="name-user font-bold">
+                          {getCookie('username')?.split(' ')[0]}
+                        </span>
+                      </div>
+                    </CSheet>
+                  </div>
+                </div>
+              </header>
+              <section className="content-container">{children}</section>
+            </div>
+          </main>
+
+          {/* richColor for colored toast*/}
+          <Toaster expand={true} richColors={true} />
+        </body>
+      </html>
+    </ReduxProvider>
   )
 }
 
