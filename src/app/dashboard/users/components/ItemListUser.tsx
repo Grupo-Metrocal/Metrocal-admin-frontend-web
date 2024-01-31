862de1e3a1d113d12bd498773d84563dce9ead72
Francisco Garcia <jjjchico1@gmail.com>
2024-01-31 11:39:10 -0600

    fix: Refactor user role assignment logic

--- src/app/dashboard/users/components/ItemListUser.tsx
+++ src/app/dashboard/users/components/ItemListUser.tsx
@@ -10,20 +10,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { MoreHorizontal } from 'lucide-react'
-
 import metrocalLogo from 'public/metrocal.svg'
 import Image from 'next/image'
 import { Modal } from '@/components/Modal'
-import { useAppDispatch } from '@/redux/hook'
-import {
-  addUserToRole,
-  deleteUserFromRole,
-} from '@/redux/features/user/rolesSlice'
-import { addRole, removeRole } from '@/redux/features/user/usersSlice'
 import { AlertDialogModal } from '@/components/AlertDialogModal'
-import { fetchData } from '@/utils/fetch'
-import { getCookie } from 'cookies-next'
-import { toast } from 'sonner'
+import { AssingRole } from './AssingRole'
 
 type PropsUser = {
   user: IUser
@@ -172,87 +163,5 @@ export const ActionItemUser = ({
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
-
-type PropsModal = {
-  onAssign: () => void
-  roles: IRole[]
-  user: IUser
-}
-const AssingRole = ({ roles, user }: PropsModal) => {
-  const [userRoles, setUserRoles] = useState(user.roles)
-
-  const dispatch = useAppDispatch()
-
-  const handleCheck = async (role: IRole) => {
-    const exist = userRoles?.some((r: any) => r.id === role.id)
-
-    if (!exist) {
-      const response = await fetchData({
-        url: `users/assign/${user.id}/role/${role.id}`,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${getCookie('token')}`,
-        },
-      })
-
-      if (response.success) {
-        toast.success(
-          `Has asignado el rol ${role.name} al usuario ${user.username}`,
-        )
-        dispatch(addRole({ user, role }))
-        dispatch(addUserToRole({ user: { ...user, roles: [role] } }))
-      } else {
-        toast.error('Ha ocurrido un error', {
-          description: 'Si el error persiste, contacta con el administrador',
-        })
-      }
-    } else {
-      const response = await fetchData({
-        url: `users/remove/${user.id}/role/${role.id}`,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${getCookie('token')}`,
-        },
-      })
-
-      if (response.success) {
-        toast.success(
-          `Has eliminado el rol ${role.name} al usuario ${user.username}`,
-        )
-        dispatch(removeRole({ user, role }))
-        dispatch(deleteUserFromRole({ user: { ...user, roles: [role] } }))
-      } else {
-        toast.error('Ha ocurrido un error', {
-          description: 'Si el error persiste, contacta con el administrador',
-        })
-      }
-    }
-  }
-
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-2">
-        {roles.map((role) => (
-          <div key={role.id} className="flex items-center gap-2">
-            {role.name !== 'user' && (
-              <AlertDialogModal
-                useCheckbox={true}
-                key={role.id}
-                onConfirm={() => handleCheck(role)}
-                id={role.name}
-                nameCheckbox={role.name}
-                checked={userRoles?.some((r: any) => r.id === role.id)}
-                label={role.name}
-                title="¿Estas seguro de querer cambiar los permisos de este usuario?"
-              />
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
   )
 }
