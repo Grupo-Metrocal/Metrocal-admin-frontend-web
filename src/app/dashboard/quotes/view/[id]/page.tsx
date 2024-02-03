63f64f60087c2109e8f47c55ae2a26883ffdeade
Francisco Garcia <jjjchico1@gmail.com>
2024-02-02 18:29:35 -0600

    feat: Add formatPrice function and update UI components

--- src/app/dashboard/quotes/view/[id]/page.tsx
+++ src/app/dashboard/quotes/view/[id]/page.tsx
@@ -8,6 +8,7 @@ import type { IQuote } from '../../requests/[id]/page'
 import { useEffect, useState } from 'react'
 import { formatDate } from '@/utils/formatDate'
 import { Spinner } from '@/components/Spinner'
+import { formatPrice } from '@/utils/formatPrice'
 
 const getData = async (id: string) => {
   const response = await fetchData({
@@ -57,6 +58,9 @@ export default function Page({ params }: IRoot) {
         title="Información principal"
         colorTitle="green"
         className="quote-viewer__main-info"
+        titleStyle={{
+          fontSize: '1.5em',
+        }}
       >
         <div className="quote-viewer__main-info__content">
           {!data ? (
@@ -64,34 +68,66 @@ export default function Page({ params }: IRoot) {
           ) : (
             <>
               <TextInfo title="Número de cotización" value={data?.no} />
-              <TextInfo title="Precio total" value={`${data?.price} $`} />
+              <TextInfo title="Precio total" value={formatPrice(data?.price)} />
+              <TextInfo title="IVA" value={`${data?.tax} %`} />
               <TextInfo
                 title="Descuento total"
                 value={`${data?.general_discount} %`}
               />
               <TextInfo
+                title="Traslado de equipo"
+                value={formatPrice(data?.extras)}
+              />
+              <TextInfo
                 title="Aprobado por"
                 value={data?.approved_by?.username || 'Aun no ha sido aprobada'}
+              />
+
+              <TextInfo
+                title="Fecha de creación"
+                value={formatDate(data?.created_at)}
+              />
+              <TextInfo
+                title="Estado"
+                value={
+                  data?.status === 'done'
+                    ? 'Aprobado'
+                    : data?.status === 'pending'
+                    ? 'Pendiente'
+                    : 'Rechazado'
+                }
               />
             </>
           )}
         </div>
 
         <div className="quote-viewer__main-info__content-equipment">
-          <span>Equipos solicitados</span>
-          <div className="items">
+          <span
+            style={{
+              fontSize: '1.5em',
+            }}
+          >
+            Equipos solicitados
+          </span>
+          <ul className="items">
             {!data ? (
               <Spinner />
             ) : (
-              data?.equipment_quote_request.map((equipment) => (
-                <TextInfo
-                  key={equipment.id}
-                  title={equipment.name}
-                  value={`Cantidad: ${equipment.count}`}
-                />
+              data?.equipment_quote_request.map((equipment, index) => (
+                <li key={index}>
+                  <EquipmentInfo
+                    key={equipment.id}
+                    equipment={equipment.name}
+                    price={formatPrice(equipment.price)}
+                    discount={`${equipment.discount} %`}
+                    status={equipment.status}
+                    count={equipment.count}
+                    total={formatPrice(equipment.total)}
+                  />
+                </li>
               ))
             )}
-          </div>
+          </ul>
         </div>
       </Content>
 
@@ -99,7 +135,10 @@ export default function Page({ params }: IRoot) {
         <Content
           title="Cliente"
           colorTitle="blue"
-          className="aside-info__content "
+          className="aside-info__content"
+          titleStyle={{
+            fontSize: '1.5em',
+          }}
         >
           <div className="aside-info__content__info">
             {!data ? (
@@ -151,5 +190,52 @@ const TextInfo = ({ title, value }: TextInfoProps) => (
   <div className="text-info">
     <h3>{title}</h3>
     <p>{value}</p>
+  </div>
+)
+
+type EquipmentInfoProps = {
+  equipment: string
+  price: string
+  discount: string
+  count: number
+  total: string
+  status: string
+}
+const EquipmentInfo = ({
+  equipment,
+  price,
+  discount,
+  count,
+  total,
+  status,
+}: EquipmentInfoProps) => (
+  <div className="equipment-info">
+    <div className="equipment-info__title">
+      <h2>{equipment}</h2>
+    </div>
+    <div className="equipment-info__content">
+      <p>
+        Precio U: <span>{price}</span>
+      </p>
+      <p>
+        Descuento: <span>{discount}</span>
+      </p>
+      <p>
+        Cantidad: <span>{count}</span>
+      </p>
+      <p>
+        TOTAL: <span>{total}</span>
+      </p>
+      <p>
+        Estado:{' '}
+        <span>
+          {status === 'done'
+            ? 'Aprobado'
+            : status === 'pending'
+            ? 'Pendiente'
+            : 'Rechazado'}
+        </span>
+      </p>
+    </div>
   </div>
 )
