4f8def82dfb41d272000e0b9af7ced3d95e6aed3
Francisco Garcia <jjjchico1@gmail.com>
2024-02-05 10:34:51 -0600

    Update content in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { LayoutPage } from '@/components/LayoutPage'
 export default function Page() {
   return (
     <LayoutPage title="Panel de control">
-      <span>contenido</span>
+      <span>En desarrollo...</span>
     </LayoutPage>
   )
 }
