9b9ab30803031ed9dab43039400e2eb2895d4623
Francisco Garcia <jjjchico1@gmail.com>
2024-02-07 23:37:08 -0600

    feat: Add MultiSlider component

---
+++ src/components/MultiSlider/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Image from 'next/image'
+
+interface MultiSliderProps {
+  children?: React.ReactNode
+}
+export const MultiSlider = ({ children }: MultiSliderProps) => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
+  return (
+    <div className="mx-auto max-w-screen-lg">
+      <Slider {...settings}>{children}</Slider>
+    </div>
+  )
+}
