00e533cfcbab60997ce4eba2c4636ce2375d83f4
Francisco Garcia <jjjchico1@gmail.com>
2024-02-19 15:12:21 -0600

    feat: Add interfaces for quote, equipment quote request, client, and activity

---
+++ src/app/dashboard/activities/interface/quote.ts
@@ -0,0 +1,54 @@
+export interface IQuote {
+  id: number
+  status: string
+  general_discount: number
+  tax: number
+  price: number
+  no: string
+  rejected_comment: any
+  rejected_options: any
+  extras: number
+  created_at: string
+  updated_at: any
+  equipment_quote_request: EquipmentQuoteRequest[]
+  client: Client
+  activity: Activity
+}
+
+export interface EquipmentQuoteRequest {
+  id: number
+  name: string
+  type_service: string
+  count: number
+  model: string
+  measuring_range: string
+  calibration_method: string
+  additional_remarks: string
+  discount: number
+  status: string
+  comment: string
+  price: number
+  total: number
+  method_id: number
+  review_status: string
+}
+
+export interface Client {
+  id: number
+  company_name: string
+  phone: string
+  address: string
+  no_ruc: string
+  email: string
+  requested_by: string
+  created_at: string
+}
+
+export interface Activity {
+  id: number
+  status: string
+  progress: number
+  created_at: string
+  updated_at: any
+  responsable: number
+}
