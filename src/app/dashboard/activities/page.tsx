f4c6ad6d38a2cbdef389df827c0756d2ae17ac2e
Francisco Garcia <jjjchico1@gmail.com>
2024-02-19 15:12:12 -0600

    feat: Add quotes carousel and fetch quotes by status

--- src/app/dashboard/activities/page.tsx
+++ src/app/dashboard/activities/page.tsx
@@ -9,6 +9,9 @@ import { Spinner } from '@/components/Spinner'
 import { Content } from '@/components/Content'
 import { ActivityItem } from './components/ActivityItem'
 import { toast } from 'sonner'
+import { IQuote } from './interface/quote'
+import { CarouselComp } from '@/components/Carousel'
+import { CarouselItemComp } from '@/components/Carousel/CarouselItem'
 
 const getData = async () => {
   const response = await fetchData({
@@ -21,8 +24,21 @@ const getData = async () => {
 
   return response
 }
+
+const getQuotes = async (status: string) => {
+  const response = await fetchData({
+    url: `quotes/get-by-status/${status}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+    },
+  })
+
+  return response
+}
 export default function Page() {
   const [activities, setActivities] = useState<IActivity[]>([])
+  const [quotes, setQuotes] = useState<IQuote[]>([])
   const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
@@ -35,11 +51,44 @@ export default function Page() {
       }
       setLoading(false)
     })
+
+    getQuotes('done').then((response) => {
+      if (response.success) {
+        setQuotes(response.data)
+      } else {
+        setQuotes([] as any)
+        toast.error(response.message || response.details)
+      }
+      setLoading(false)
+    })
   }, [])
 
   return (
     <LayoutPage title="Actividades">
       <Content title="Asignación de actividades">
+        <div className="activities-quotes-carousel">
+          <CarouselComp>
+            <CarouselItemComp>
+              {quotes && quotes.length > 0 ? (
+                quotes.map((quote: IQuote) => (
+                  <div key={quote.id} className="quote-item">
+                    <h3>Cotización: {quote.no}</h3>
+                    <p>Cliente: {quote.client.company_name}</p>
+                    <p>
+                      Fecha: {new Date(quote.created_at).toLocaleDateString()}
+                    </p>
+                    <p>Precio: {quote.price}</p>
+                  </div>
+                ))
+              ) : (
+                <div className="quote-item">
+                  <p>No hay cotizaciones</p>
+                </div>
+              )}
+            </CarouselItemComp>
+          </CarouselComp>
+        </div>
+
         <div className="activities-page">
           <header className="activities-header">
             <h2>Actividades disponibles: {activities.length}</h2>
