0fb3c6e2df1f1318d8cff475425b5c7d8afd8413
Francisco Garcia <jjjchico1@gmail.com>
2024-03-06 15:05:29 -0600

    feat: Add pending certificates functionality

--- src/app/dashboard/certificates/page.tsx
+++ src/app/dashboard/certificates/page.tsx
@@ -1,14 +1,54 @@
+'use client'
 import './page.scss'
 import { LayoutPage } from '@/components/LayoutPage'
 import certificateIcon from '@/assets/icons/certificate_icon.svg'
 import statsIcon from '@/assets/icons/stats.svg'
 import activitiesPendingIcon from '@/assets/icons/certificate_pending_icon.svg'
 import { StatisticsCard } from '../../../components/StatisticsCard'
-
+import { getCookie } from 'cookies-next'
+import { IPendingActivities } from './interface/pendingActivities'
 import { Content } from '@/components/Content'
 import { formatPrice } from '@/utils/formatPrice'
+import { fetchData } from '@/utils/fetch'
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
+import { Spinner } from '@/components/Spinner'
+import { ItemPendingCertify } from './components/itemPendingCertify'
+
+const getData = async () => {
+  return await fetchData({
+    url: 'activities/done',
+    method: 'GET',
+    headers: {
+      Autorization: `Bearer ${getCookie('token')}`,
+    },
+  })
+}
 
 export default function Page() {
+  const [pendingActivities, setPendingActivities] = useState<
+    IPendingActivities[]
+  >([])
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getData()
+      .then((data) => {
+        if (data.success) {
+          setPendingActivities(data.data)
+        } else {
+          toast.error('Error al cargar los datos')
+        }
+      })
+      .catch((error) => {
+        toast.error('Error al cargar los datos')
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <LayoutPage title="Certificados">
       <Content
@@ -45,8 +85,36 @@ export default function Page() {
         </div>
       </Content>
 
-      <Content title="Certificados" colorTitle="yellow" className="mt-4">
-        <div></div>
+      <Content
+        title="Certificados pendientes"
+        colorTitle="yellow"
+        className="mt-4"
+      >
+        <div className="pending-certificate">
+          <div className="pending-certificate__table">
+            <header>
+              <span>Responsable</span>
+              <span>Detalles</span>
+            </header>
+
+            <div className="pending-certificate__table__content">
+              {loading ? (
+                <div className="flex mt-4 justify-center">
+                  <Spinner />
+                </div>
+              ) : pendingActivities.length > 0 ? (
+                pendingActivities.map((activity) => (
+                  <ItemPendingCertify key={activity.id} activity={activity} />
+                ))
+              ) : (
+                <p className="text-center mt-4">
+                  No hay actividades pendientes para certificar
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="pending-certificate__details"></div>
+        </div>
       </Content>
     </LayoutPage>
   )
