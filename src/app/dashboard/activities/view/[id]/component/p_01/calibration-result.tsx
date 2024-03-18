0d0e4bd7776e82ab1550e6422e72b267e1546aa3
Francisco Garcia <jjjchico1@gmail.com>
2024-03-18 09:19:31 -0600

    Refactor calibration result component layout

--- src/app/dashboard/activities/view/[id]/component/p_01/calibration-result.tsx
+++ src/app/dashboard/activities/view/[id]/component/p_01/calibration-result.tsx
@@ -13,21 +13,18 @@ export const CalibrationResults = ({
     <div className="calibration-results">
       {calibration_results?.results.map((result) => {
         return (
-          <div
-            key={result.cicle_number}
-            className="flex flex-col gap-4 border p-4"
-          >
+          <div key={result.cicle_number} className="flex flex-col gap-4 p-4">
             <div className="header">
               <div className="grid grid-cols gap-4 ">
                 <span className="text-center font-semibold">
                   Ciclo {result.cicle_number}
                 </span>
               </div>
-              <div className="grid grid-cols-2 gap-4 border p-2">
+              <div className="grid grid-cols-2 gap-4 border p-2 text-center">
                 <span>Ascendente</span>
                 <span>Descendente</span>
               </div>
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-2 gap-4 text-center">
                 <div className="grid grid-cols-2 gap-4 border p-2">
                   <span>Patron</span>
                   <span>Equipo</span>
@@ -38,7 +35,7 @@ export const CalibrationResults = ({
                 </div>
               </div>
             </div>
-            <div className="body">
+            <div className="body text-center">
               {result.calibration_factor.map((calibration_factor, index) => {
                 return (
                   <CalibrationRow
@@ -61,7 +58,7 @@ const CalibrationRow = ({
   calibration_factor: CalibrationFactor
 }) => {
   return (
-    <div className="grid grid-cols-2 gap-4 p-2">
+    <div className="grid grid-cols-2 gap-4 p-2 border font-semibold">
       <div className="grid grid-cols-2 gap-4">
         <span>{calibration_factor.upward.pattern}</span>
         <span>{calibration_factor.upward.equipment}</span>
