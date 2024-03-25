388f87e91423ca46a8d315d67e6f24bc00c4da09
Francisco Garcia <jjjchico1@gmail.com>
2024-03-25 17:30:07 -0600

    Add page for updating activities view

---
+++ src/app/dashboard/activities/view/update/[[...slug]]/page.tsx
@@ -0,0 +1,73 @@
+import './index.scss'
+import { LayoutPage } from '@/components/LayoutPage'
+import { P_01 } from './component/p_01'
+import { fetchData } from '@/utils/fetch'
+
+const getEquipment = async (id: string, calibration_method: string) => {
+  try {
+    const response = await fetchData({
+      url: `methods/${calibration_method.toLowerCase()}/equipment/${id}`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    console.log({ response })
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export interface IRoot {
+  params: {
+    slug: string[]
+  }
+}
+
+const RENDERER_METHOD = {
+  'NI-MCIT-P-01': P_01,
+}
+export default async function Page({ params }: IRoot) {
+  const { slug } = params
+  const id = slug[0]
+  const calibration_method = slug[1]
+
+  const Renderer =
+    RENDERER_METHOD[calibration_method as keyof typeof RENDERER_METHOD]
+
+  const equipment = await getEquipment(id, calibration_method)
+  console.log({ equipment })
+
+  return (
+    <LayoutPage
+      title={`Modificación de resultados en equipo ${calibration_method}`}
+      rollBack
+    >
+      {Renderer ? (
+        equipment.data ? (
+          <Renderer equipment={equipment.data} />
+        ) : (
+          <div className="w-full h-full flex items-center justify-center">
+            <h1
+              className="text-2xl font-bold text-center text-red-500
+          "
+            >
+              No se encontró el equipo con el método de calibración{' '}
+              {calibration_method}
+            </h1>
+          </div>
+        )
+      ) : (
+        <div className="w-full h-full flex items-center justify-center">
+          <h1
+            className="text-2xl font-bold text-center text-red-500
+          "
+          >
+            El método de calibración no existe
+          </h1>
+        </div>
+      )}
+    </LayoutPage>
+  )
+}
