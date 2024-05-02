dd28bb63f087d2624a7a3bbe4d39444b94b72363
Francisco Garcia <jjjchico1@gmail.com>
2024-05-02 14:37:45 -0600

    Add ReviewActivity component to activity view page

---
+++ src/app/dashboard/activities/view/[id]/component/ReviewActivity.tsx
@@ -0,0 +1,52 @@
+import { AlertDialogModal } from '@/components/AlertDialogModal'
+import { fetchData } from '@/utils/fetch'
+import { getCookie } from 'cookies-next'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+
+const sendReview = async (activityID: number) => {
+  const response = await fetchData({
+    url: `activities/review-activity/${activityID}`,
+    method: 'POST',
+    body: {
+      token: getCookie('token'),
+    },
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  return response
+}
+
+export const ReviewActivity = ({ activityID }: { activityID: number }) => {
+  const router = useRouter()
+
+  const handleConfirmReview = async () => {
+    toast.loading('Confirmando...')
+    const response = await sendReview(activityID)
+
+    toast.dismiss()
+
+    if (response.success) {
+      toast.success('Actividad revisada correctamente')
+      router.push('/dashboard/activities')
+    } else {
+      toast.error('Actividad en proceso', {
+        description: response.details,
+      })
+    }
+  }
+
+  return (
+    <div className="flex justify-end mt-4">
+      <AlertDialogModal
+        onConfirm={handleConfirmReview}
+        nameButton="Confirmar revisión"
+        title="Revisión de actividad"
+        description="Al confirmar la revisión de la actividad, esta se marcará como revisada y habilitaras la emición de certificados"
+      />
+    </div>
+  )
+}
