e466374d4574fb8a659a68d322b9dfcc2e55b0bf
Francisco Garcia <jjjchico1@gmail.com>
2024-05-14 14:04:03 -0600

    Update page titles for records of activities and certificates

--- src/app/dashboard/activities/records/page.tsx
+++ src/app/dashboard/activities/records/page.tsx
@@ -2,7 +2,7 @@ import { LayoutPage } from '@/components/LayoutPage'
 
 export default function RecordsPage() {
   return (
-    <LayoutPage title="Registros de actividad">
+    <LayoutPage title="Registros de actividad" rollBack>
       <h1>Records</h1>
     </LayoutPage>
   )
--- src/app/dashboard/certificates/records/page.tsx
+++ src/app/dashboard/certificates/records/page.tsx
@@ -2,7 +2,7 @@ import { LayoutPage } from '@/components/LayoutPage'
 
 export default function RecordsPage() {
   return (
-    <LayoutPage title="Registros de certificaciones emitidas">
+    <LayoutPage title="Registros de certificaciones emitidas" rollBack>
       <h1>Records</h1>
     </LayoutPage>
   )
