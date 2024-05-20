349da68faaa2988405872e3a7924f809fcc710ef
Francisco Garcia <jjjchico1@gmail.com>
2024-05-20 10:02:55 -0600

    feat: Add T_03 method to activity update page

---
+++ src/app/dashboard/activities/view/update/methods/t_03/index.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { TabsNavigations } from '@/components/Tabs'
+import { IT_03 } from '../../../[id]/interface/t_03'
+import { Content } from '@/components/Content'
+import { fetchData } from '@/utils/fetch'
+import { getCookie } from 'cookies-next'
+import { toast } from 'sonner'
+import { EquipmentInformation } from './component/equipment_information'
+
+export const T_03 = ({
+  equipment,
+  activity_id,
+}: {
+  equipment: IT_03
+  activity_id: string
+}) => {
+  const handleSaveInformation = async (
+    values: any,
+    url: string,
+    useActivityID?: boolean,
+  ) => {
+    toast.loading('Guardando información')
+
+    url = `${url}${equipment.id}`
+
+    if (useActivityID) {
+      url += `/${activity_id}`
+    }
+
+    const response = await fetchData({
+      url,
+      method: 'POST',
+      body: values,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getCookie('token')}`,
+      },
+    })
+
+    toast.dismiss()
+
+    if (response.success) {
+      toast.success('Información guardada correctamente')
+    } else {
+      toast.error('No se ha podido guardar la información')
+    }
+  }
+
+  return (
+    <Content title="Al guardar cada informacion modificada aumentara en 1 el contador de modificaciones">
+      <TabsNavigations
+        items={[
+          {
+            value: 'equipment_information',
+            label: 'Información del equipo',
+            Component: () => (
+              <EquipmentInformation
+                equipment={equipment.equipment_information}
+                handleSaveInformation={handleSaveInformation}
+              />
+            ),
+          },
+        ]}
+      />
+    </Content>
+  )
+}
