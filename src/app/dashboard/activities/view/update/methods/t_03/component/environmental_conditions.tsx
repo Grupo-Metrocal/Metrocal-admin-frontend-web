b63592a713cc2e5a7b27d29280a31765e4089e74
Francisco Garcia <jjjchico1@gmail.com>
2024-05-20 10:26:53 -0600

    feat: Add EnvironmentalConditions component to T_03 method in activity update page

---
+++ src/app/dashboard/activities/view/update/methods/t_03/component/environmental_conditions.tsx
@@ -0,0 +1,68 @@
+import { useForm } from '@/hooks/useForm'
+import { IEnvironmentalConditions } from '../../../../[id]/interface/t_03'
+import { AlertDialogModal } from '@/components/AlertDialogModal'
+import { AutocompleteInput } from '@/components/AutocompleteInput'
+import { CInput } from '@/components/CInput'
+
+export const EnvironmentalConditions = ({
+  environmentalConditions,
+  handleSaveInformation,
+}: {
+  handleSaveInformation: (
+    values: IEnvironmentalConditions,
+    url: string,
+    useActivityID?: boolean,
+  ) => void
+  environmentalConditions: IEnvironmentalConditions
+}) => {
+  const url = `methods/ni-mcit-t-03/environmental-conditions/`
+  const { values, handleInputChange } = useForm({ ...environmentalConditions })
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <CInput
+          label="Temperatura"
+          name="temperature"
+          value={values.temperature}
+          onChange={handleInputChange}
+          type="number"
+        />
+        <CInput
+          label="Humedad"
+          name="humidity"
+          value={values.humidity}
+          onChange={handleInputChange}
+          type="number"
+        />
+
+        <AutocompleteInput
+          requiredLabel
+          value={values.pattern}
+          label="Patron utilizado"
+          name="pattern"
+          onChange={handleInputChange}
+          required
+          placeholder="Escriba o seleccione su patron"
+          list={[
+            { id: 1, name: 'NI-MCPVE-01' },
+            { id: 2, name: 'NI-MCPVE-16' },
+          ]}
+          keyList="pattern"
+          inputType="string"
+        />
+      </div>
+      <div>
+        <AlertDialogModal
+          title="Guardar modificaciones"
+          description="¿Estás seguro de guardar las modificaciones?"
+          onConfirm={() => handleSaveInformation(values, url)}
+          nameButton="Guardar modificaciones"
+          buttonStyle={{
+            margin: '1em 0',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
