e6221b05c4ca6b2fb96b6dbb49a07dfe63a3cd9b
Francisco Garcia <jjjchico1@gmail.com>
2024-05-20 10:02:43 -0600

    feat: Add interfaces for certificates records page

---
+++ src/app/dashboard/activities/view/[id]/interface/t_03.ts
@@ -0,0 +1,71 @@
+export interface IT_03 {
+  id: number
+  calibration_location: string
+  report_status: boolean
+  report_messages: any[]
+  status: string
+  certificate_id: string
+  certificate_code: string
+  certificate_url: string
+  review_state: boolean
+  review_user_id: any
+  created_at: string
+  updated_at: string
+  equipment_information: IEquipmentInformation
+  environmental_conditions: EnvironmentalConditions
+  description_pattern: DescriptionPattern
+  calibration_results: CalibrationResults
+}
+
+export interface IEquipmentInformation {
+  id: number
+  device: string
+  maker: string
+  serial_number: string
+  measurement_range: any
+  temperature_min: number
+  temperature_max: number
+  unit: string
+  model: string
+  code: string
+  resolution: number
+  sensor: string
+}
+
+export interface EnvironmentalConditions {
+  id: number
+  temperature: number
+  humidity: number
+  pattern: string
+}
+
+export interface DescriptionPattern {
+  id: number
+  pattern: string
+  observation: string
+  creditable: boolean
+}
+
+export interface CalibrationResults {
+  id: number
+  results: Result[]
+}
+
+export interface Result {
+  cicle_number: number
+  calibration_factor: CalibrationFactor[]
+}
+
+export interface CalibrationFactor {
+  upward: Upward
+  pattern: number
+  downward: Downward
+}
+
+export interface Upward {
+  equipment: number
+}
+
+export interface Downward {
+  equipment: number
+}
