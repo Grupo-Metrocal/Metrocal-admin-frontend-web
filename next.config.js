c130ff1b6863f2b2300df03e97afb468d3a3f306
Francisco Garcia <jjjchico1@gmail.com>
2024-05-20 18:09:41 -0600

    chore: Update next.config.js to include remotePatterns for image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'app-grupometrocal.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
   },
 }
 
