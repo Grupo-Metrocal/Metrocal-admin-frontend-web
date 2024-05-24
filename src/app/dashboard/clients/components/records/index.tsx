08f1c0022ce42ec8eb2582fd9cea9b0f1381a6a0
Francisco Garcia <jjjchico1@gmail.com>
2024-05-24 11:54:18 -0600

    feat: Add ClientRecords component for displaying client records in the dashboard

---
+++ src/app/dashboard/clients/components/records/index.tsx
@@ -0,0 +1,74 @@
+import { DataTableDemo } from '@/components/Table'
+import { ColumnsCertifiedRecords } from '../columnsRecords.tsx'
+
+interface IProps {
+  records: any[]
+  currentPage: number
+  pagination: {
+    current_page: number
+    total_pages: number
+    total_data: number
+  }
+  setCurrentPage: any
+  loading: boolean
+}
+
+export type IClientsRecordsTable = {
+  id: number
+  company_name: string
+  email: string
+  requested_by: string
+  phone: string
+  address: string
+  quote_requests: number[]
+}
+
+export const ClientRecords = ({
+  records,
+  currentPage,
+  pagination,
+  setCurrentPage,
+  loading,
+}: IProps) => {
+  const handlePreviousPage = () => {
+    if (pagination.current_page > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  const handleNextPage = () => {
+    if (pagination.current_page < pagination.total_pages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  return (
+    <div className="bg-white p-4 rounded-lg">
+      {
+        <DataTableDemo<IClientsRecordsTable>
+          columns={ColumnsCertifiedRecords({ onDelete: () => {} })}
+          searchValue={''}
+          data={records ?? []}
+          search_by="client_company_name"
+          setPagination={(event: { target: { value: any } }) => {
+            setCurrentPage(event.target.value)
+          }}
+          handleNextPage={handleNextPage}
+          handlePreviousPage={handlePreviousPage}
+          currentPage={currentPage}
+          totalPages={pagination.total_pages}
+          isLoading={loading}
+          search_placeholder="Buscar nombre de client"
+          filter_columns={{
+            company_name: 'Empresa',
+            email: 'Correo del cliente',
+            requested_by: 'Solicitante',
+            phone: 'Teléfono',
+            address: 'Dirección',
+            quote_requests: 'Cotizaciones solicitadas',
+          }}
+        />
+      }
+    </div>
+  )
+}
