5e4353d1959f5ac1bb635e55c36761cb6eaa1b93
Francisco Garcia <jjjchico1@gmail.com>
2024-05-27 13:28:22 -0600

    feat: Add client details page to dashboard

---
+++ src/app/dashboard/clients/view/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { LayoutPage } from '@/components/LayoutPage'
+
+export default function Page() {
+  return (
+    <LayoutPage title="Detalles del cliente">
+      <div></div>
+    </LayoutPage>
+  )
+}
