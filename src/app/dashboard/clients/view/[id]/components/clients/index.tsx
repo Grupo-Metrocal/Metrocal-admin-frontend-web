dbb7bb349aec8939c1a2f2c0eae8bf2197246910
francisco-JGC <jjjchico1@gmail.com>
2024-06-10 10:17:53 -0600

    refactor: Update ClientRenderer component to include pencil icon for editing client information

--- src/app/dashboard/clients/view/[id]/components/clients/index.tsx
+++ src/app/dashboard/clients/view/[id]/components/clients/index.tsx
@@ -1,12 +1,22 @@
 import { IClient } from '@/app/contactInformation'
+import pencilIcon from '@/assets/icons/pencil.svg'
 import './index.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface IProps {
   client: IClient
 }
 export const ClientRenderer = ({ client }: IProps) => {
   return (
-    <div className="renderer-client">
+    <div className="renderer-client relative">
+      <Link
+        href={`/dashboard/clients/update/${client.id}`}
+        passHref
+        className="bg-[#09f] p-2 rounded-full w-fit absolute right-0 top-0 hover:bg-[#0af] cursor-pointer"
+      >
+        <Image src={pencilIcon} alt="pencil icon" />
+      </Link>
       <Item label="Empresa" value={client.company_name} />
       <Item label="Solicitante" value={client.requested_by} />
       <Item label="Email" value={client.email} />
