4ffd90f59cd3658d1e1f0833029e0f4957ca60a8
francisco-JGC <jjjchico1@gmail.com>
2024-06-11 10:50:28 -0600

    refactor: Update CertifiedRecords component to include search functionality

--- src/app/dashboard/certificates/records/page.tsx
+++ src/app/dashboard/certificates/records/page.tsx
@@ -7,10 +7,11 @@ import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { CertifiedRecords } from './components/records'
 import { StatisticsActivity } from './components/statistics'
+import { useForm } from '@/hooks/useForm'
 
-const getRecords = async (page: number) => {
+const getRecords = async (page: number, company_name?: string) => {
   return await fetchData({
-    url: `activities/certified-activities/${page}/10`,
+    url: `activities/certified-activities/${page}/10/${company_name}`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -23,11 +24,39 @@ export default function RecordsPage() {
   const [records, setRecords] = useState<any>([])
   const [loading, setLoading] = useState<boolean>(true)
   const [currentPage, setCurrentPage] = useState<number>(1)
+  const { values, handleInputChange } = useForm({
+    search: '',
+  })
   const [pagination, setPagination] = useState<any>({
     current_page: 0,
     total_pages: 0,
     total_data: 0,
   })
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      getRecords(currentPage, values.search)
+        .then((data) => {
+          if (data.success) {
+            setRecords(data.data)
+            setPagination({
+              current_page: data.current_page,
+              total_pages: data.total_pages,
+              total_data: data.total_data,
+            })
+          } else {
+            setRecords([])
+            toast.error('No se pudieron cargar los registros')
+          }
+        })
+        .finally(() => {
+          toast.dismiss()
+          setLoading(false)
+        })
+    }, 700)
+
+    return () => clearTimeout(timeOut)
+  }, [values, currentPage])
 
   useEffect(() => {
     toast.loading('Cargando registros...')
@@ -65,6 +94,8 @@ export default function RecordsPage() {
             {
               <CertifiedRecords
                 records={records}
+                searchValue={values.search}
+                handleInputChange={handleInputChange}
                 currentPage={currentPage}
                 pagination={pagination}
                 setPagination={setPagination}
