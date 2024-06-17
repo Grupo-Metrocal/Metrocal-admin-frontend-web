eb6f62ae65da8add055771bf3452bd8ba24ca2bf
francisco-JGC <jjjchico1@gmail.com>
2024-06-17 10:09:39 -0600

    feat: Add EnvironmentalConditions component for V_01 activity method in activities view

---
+++ src/app/dashboard/activities/view/[id]/component/v_01/environmental-conditions.tsx
@@ -0,0 +1,110 @@
+import { ReportMethodActivity } from '@/components/ReportMethodActivity'
+import { IEnvironmentalConditions } from '../../interface/v_01'
+
+interface EnvironmentalConditionsProps {
+  environmental_conditions: IEnvironmentalConditions
+  id: number
+  method_name: string
+  report_status: boolean
+  report_messages: string[]
+}
+
+export const EnvironmentalConditions = ({
+  environmental_conditions,
+  id,
+  method_name,
+  report_status,
+  report_messages,
+}: EnvironmentalConditionsProps) => {
+  return (
+    <div className="flex flex-col space-y-4 ">
+      <div className="flex flex-col space-y-4 gap-4 max-h-[70vh] overflow-y-auto">
+        {environmental_conditions?.points?.map((point, index) => {
+          return (
+            <table key={index}>
+              <thead>
+                <tr className="bg-gray-200">
+                  <th>Punto {point.point_number}</th>
+                  <th className="border px-4 py-2">Iniciales</th>
+                  <th className="border px-4 py-2">Finales</th>
+                  <th className="border px-4 py-2">Resolución</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    <p>Temperatura ºC</p>
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.temperature.initial}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.temperature.final}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.temperature.resolution}
+
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    <p>Humedad Relativa %</p>
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.humidity.initial}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.humidity.final}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.humidity.resolution}
+
+                  </td>
+                </tr>
+
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    <p>Presión Pa</p>
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.presion_pa.initial}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.presion_pa.final}
+
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+
+                    {point.presion_pa.resolution}
+
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          )
+        })}
+      </div>
+
+      <ReportMethodActivity
+        method_name={method_name}
+        zone={'Condiciones ambientales'}
+        method_id={id}
+        report_messages={report_messages}
+        report_status={report_status}
+      />
+    </div>
+  )
+}
