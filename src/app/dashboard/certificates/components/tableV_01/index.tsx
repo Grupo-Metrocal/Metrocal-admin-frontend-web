bff4ab527115a51087fa9cf8cc8c2fb90b744ce2
francisco-JGC <jjjchico1@gmail.com>
2024-06-17 12:14:35 -0600

    feat: Add TableV_01 component for certificates page

---
+++ src/app/dashboard/certificates/components/tableV_01/index.tsx
@@ -0,0 +1,156 @@
+import './index.scss'
+import { ICertificate_V_01 } from '../../interface/v-01'
+
+export const TableV_01 = ({
+  certificate,
+}: {
+  certificate: ICertificate_V_01
+}) => {
+  return (
+    <div className="table-v-01">
+      <section className="table-v-01__equipment-information">
+        <h2>Información del equipo</h2>
+
+        <div className="table-v-01__equipment-information__content">
+          <div>
+            <p>Código de servicio:</p>
+            <span>{certificate.equipment_information.service_code}</span>
+          </div>
+          <div>
+            <p>Fecha de emisión del certificado:</p>
+            <span>
+              {certificate.equipment_information.certificate_issue_date}
+            </span>
+          </div>
+          <div>
+            <p>Fecha de calibración:</p>
+            <span>{certificate.equipment_information.calibration_date}</span>
+          </div>
+          <div>
+            <p>Equipo calibrado:</p>
+            <span>{certificate.equipment_information.device}</span>
+          </div>
+          <div>
+            <p>Fabricante / Marca:</p>
+            <span>{certificate.equipment_information.maker}</span>
+          </div>
+          <div>
+            <p>No. Serie:</p>
+            <span>{certificate.equipment_information.serial_number}</span>
+          </div>
+          <div>
+            <p>Cap. nominal/alcance:</p>
+            <span>{certificate.equipment_information.nominal_range}</span>
+          </div>
+          <div>
+            <p>División de escala:</p>
+            <span>{certificate.equipment_information.scale_division}</span>
+          </div>
+          <div>
+            <p>Código:</p>
+            <span>{certificate.equipment_information.code}</span>
+          </div>
+          <div>
+            <p>Solicitante:</p>
+            <span>{certificate.equipment_information.applicant}</span>
+          </div>
+          <div>
+            <p>Dirección del solicitante:</p>
+            <span>{certificate.equipment_information.address}</span>
+          </div>
+          <div>
+            <p>Ubicación de calibración:</p>
+            <span>
+              {certificate.equipment_information.calibration_location}
+            </span>
+          </div>
+        </div>
+      </section>
+
+      <section className="table-v-01__calibration-result">
+        <h2>Tabla de resultados de calibración</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Volumen Nominal</th>
+              <th>Volumen Convencional</th>
+              <th>Desviación</th>
+              <th>Incertidumbre expandida K = 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            {certificate.calibration_results.nominal_volume.map(
+              (item: any, index: any) => (
+                <tr key={index}>
+                  <td>{item}</td>
+                  <td>
+                    {
+                      certificate.calibration_results.conventional_volume[index]
+                    }
+                  </td>
+                  <td>
+                    {certificate.calibration_results.desviation[index]}
+                  </td>
+                  <td>
+                    {
+                      certificate.calibration_results.uncertainty[index]
+                    }
+                  </td>
+                </tr>
+              ),
+            )}
+          </tbody>
+        </table>
+      </section>
+
+      <section className="table-v-01__result_unid_system">
+        <section className="table-v-01__environmental-conditions">
+          <h2>
+            <span>
+              Condiciones ambientales
+            </span>
+          </h2>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Temperatura</th>
+                <th>Humedad relativa</th>
+                <th>Presión atmosférica</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{certificate.environmental_conditions.temperature}</td>
+                <td>{certificate.environmental_conditions.humidity}</td>
+                <td>{certificate.environmental_conditions.presion}</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+
+        <section className="table-v-01__observations">
+          <h2>Observaciones</h2>
+
+          {/* <div>
+            <p>Patron utilizado</p>
+            <span>{certificate.description_pattern?.pattern}</span>
+          </div> */}
+
+          <div>
+            <p>
+              Este certificado{' '}
+              {certificate.creditable ? 'es acreditado ✅' : 'no es acreditado'}
+            </p>
+          </div>
+
+          <div>
+            <p>Observaciones adicionales</p>
+            <span>{certificate.observations}</span>
+          </div>
+        </section>
+      </section>
+    </div>
+  )
+}
