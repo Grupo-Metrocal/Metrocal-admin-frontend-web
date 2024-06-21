7a83473ebe8637919183d614216f131a5a4dc71c
francisco-JGC <jjjchico1@gmail.com>
2024-06-21 11:48:52 -0600

    refactor: Update equipment information display in activity page

---
+++ src/app/dashboard/activities/view/update/methods/m_01/component/equipment_information.tsx
@@ -0,0 +1,69 @@
+import { useForm } from '@/hooks/useForm'
+import { IEquipmentInformation } from '../../../../[id]/interface/m_01'
+import { CInput } from '@/components/CInput'
+import { AlertDialogModal } from '@/components/AlertDialogModal'
+import { AutocompleteInput } from '@/components/AutocompleteInput'
+
+export const EquipmentInformation = ({
+  equipment,
+  handleSaveInformation,
+}: {
+  handleSaveInformation: (
+    values: IEquipmentInformation,
+    url: string,
+    useActivityID?: boolean,
+  ) => void
+  equipment: IEquipmentInformation
+}) => {
+  const { values, handleInputChange, handleSelectChange } = useForm({
+    ...equipment,
+  })
+  const url = `methods/ni-mcit-m-01/equipment-information/`
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <AutocompleteInput
+          requiredLabel
+          value={values.calibration_object}
+          label="Dispositivo"
+          name="calibration_object"
+          onChange={handleInputChange}
+          required
+          placeholder="Escriba o seleccione su dispositivo"
+          list={[{ id: 1, name: '' }]}
+          keyList="calibration_object"
+        />
+        <CInput
+          label="Fabricante"
+          name="maker"
+          value={values.maker}
+          onChange={handleInputChange}
+        />
+        <CInput
+          label="Modelo"
+          name="model"
+          value={values.model}
+          onChange={handleInputChange}
+        />
+        <CInput
+          label="Código"
+          name="code"
+          value={values.code}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <AlertDialogModal
+          title="Guardar modificaciones"
+          description="¿Estás seguro de guardar las modificaciones?"
+          onConfirm={() => handleSaveInformation(values, url)}
+          nameButton="Guardar modificaciones"
+          buttonStyle={{
+            margin: '1em 0',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
