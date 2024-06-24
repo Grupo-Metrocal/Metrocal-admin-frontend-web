389d827430fa45e1c4525cabc078632be4250a74
francisco-JGC <jjjchico1@gmail.com>
2024-06-24 15:13:20 -0600

    fix: FormatDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,5 @@
 export const formatDate = (date: string) => {
-  const dateObj = new Date(date)
-  return dateObj.toLocaleDateString('es-ES', {
-    year: 'numeric', // 2-digit, numeric
-    month: 'short', // 2-digit, numeric, long, short, narrow
-    day: 'numeric', // 2-digit, numeric
-  })
+  const dateObj = new Date(date);
+
+  return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
 }
