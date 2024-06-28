da5bf62cadbb1d053b7c1fa0413633314f2a86d0
RACZ03 <rmorck03@gmail.com>
2024-06-27 18:12:17 -0600

    fix: fix table certification D-02

--- src/app/dashboard/certificates/components/tableD_02/index.tsx
+++ src/app/dashboard/certificates/components/tableD_02/index.tsx
@@ -1,51 +1,86 @@
-import { ICertificate_D_02 } from "../../interface/d-02"
+import './index.scss'
+import { ICertificate_D_02 } from '../../interface/d-02'
 
-export const TableD_02 = ({
-  certificate,
-}: {
-  certificate: ICertificate_D_02
-}) => {
+export const TableD_02 = ({ certificate }: { certificate: any }) => {
+  console.log('Ahora si ', certificate.certificate)
+
+  const certificateData: ICertificate_D_02 = certificate.certificate
+
   return (
-    <div className="table-d-01">
-      <section className="table-d-01__equipment-information">
+    <div className="table-d-02">
+      <section className="table-d-02__equipment-information">
         <h2>Información del equipo</h2>
 
-        <div className="table-d-01__equipment-information__content">
+        <div className="table-d-02__equipment-information__content">
           <div>
-            <p>Equipo:</p>
-            <span>{certificate.equipment_information.device}</span>
+            <p>Código de servicio:</p>
+            <span>
+              {certificateData.equipment_information.service_code || '---'}
+            </span>
+          </div>
+          <div>
+            <p>Fecha de emisión del certificado:</p>
+            <span>
+              {certificateData.equipment_information.certificate_issue_date}
+            </span>
+          </div>
+          <div>
+            <p>Fecha de calibración:</p>
+            <span>
+              {certificateData.equipment_information.calibration_date}
+            </span>
+          </div>
+          <div>
+            <p>Equipo calibrado:</p>
+            <span>
+              {certificateData.equipment_information.object_calibrated}
+            </span>
           </div>
           <div>
             <p>Fabricante / Marca:</p>
-            <span>{certificate.equipment_information.maker}</span>
+            <span>{certificateData.equipment_information.maker}</span>
           </div>
           <div>
             <p>No. Serie:</p>
-            <span>{certificate.equipment_information.serial_number}</span>
-          </div>
-          <div>
-            <p>Rango de medición:</p>
-            <span>{certificate.equipment_information.measurement_range}</span>
-          </div>
-          <div>
-            <p>Resolución:</p>
-            <span>{certificate.equipment_information.resolution}</span>
+            <span>{certificateData.equipment_information.serial_number}</span>
           </div>
           <div>
             <p>Modelo:</p>
-            <span>{certificate.equipment_information.model}</span>
+            <span>{certificateData.equipment_information.model}</span>
+          </div>
+          <div>
+            <p>Rango de medición:</p>
+            <span>
+              {certificateData.equipment_information.measurement_range}
+            </span>
+          </div>
+          <div>
+            <p>Resolución:</p>
+            <span>{certificateData.equipment_information.resolution}</span>
           </div>
           <div>
             <p>Código:</p>
-            <span>{certificate.equipment_information.code}</span>
+            <span>
+              {certificateData.equipment_information.identification_code}
+            </span>
           </div>
           <div>
-            <p>Longitud:</p>
-            <span>{certificate.equipment_information.length}</span>
+            <p>Solicitante:</p>
+            <span>{certificateData.equipment_information.applicant}</span>
+          </div>
+          <div>
+            <p>Dirección del solicitante:</p>
+            <span>{certificateData.equipment_information.address}</span>
+          </div>
+          <div>
+            <p>Ubicación de calibración:</p>
+            <span>
+              {certificateData.equipment_information.calibration_location}
+            </span>
           </div>
         </div>
       </section>
-      <section className="table-d-01__calibration-result">
+      <section className="table-d-02__calibration-result">
         <h2>Tabla de resultados de calibración</h2>
         <table>
           <thead>
@@ -58,79 +93,69 @@ export const TableD_02 = ({
             </tr>
           </thead>
           <tbody>
-            {certificate.calibration_results.calibration_accuracy_tests.map(
+            {certificateData.calibration_results.calibration_accuracy_tests.map(
               (calibrationPoint) => (
-                <tr key={calibrationPoint.nominal_value}>
-                  <td>{calibrationPoint.nominal_value}</td>
-                  <td>{calibrationPoint.actual_length}</td>
-                  <td>{calibrationPoint.current_reading}</td>
-                  <td>{calibrationPoint.deviation}</td>
-                  <td>{calibrationPoint.expanded_uncertainty}</td>
+                <tr key={calibrationPoint.C}>
+                  <td>{calibrationPoint.C}</td>
+                  <td>
+                    {calibrationPoint.F}
+                  </td>
+                  <td>
+                    {calibrationPoint.K}
+                  </td>
+                  <td>
+                    {calibrationPoint.Q}
+                  </td>
+                  <td>
+                    {calibrationPoint.W}
+                  </td>
                 </tr>
               ),
             )}
           </tbody>
         </table>
       </section>
-      <section className="table-d-01__environmental_conditions">
+      <section className="table-d-02__environmental_conditions">
         <h2>Condiciones ambientales</h2>
 
         <div>
           <div>
             <p>Temperatura ambiente:</p>
-            <span>{certificate.environmental_conditions.temperature} (°C)</span>
+            <span>
+              ({certificateData.environmental_conditions.temperature} {' ± '}
+              {certificateData.environmental_conditions.temperature2}) °C
+            </span>
           </div>
           <div>
             <p>Humedad relativa:</p>
-            <span>{certificate.environmental_conditions.humidity} (%)</span>
+            <span>
+              ({certificateData.environmental_conditions.humidity} {' ± '}
+              {certificateData.environmental_conditions.humidity2}) % HR
+            </span>
           </div>
         </div>
       </section>
-      <section className="table-d-01__used_patterns">
-        <h2>Descripción de patrones utilizados</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Equipo</th>
-              <th>Código</th>
-              <th>Certificado</th>
-              <th>Trazabilidad</th>
-              <th>Proxima calibración</th>
-            </tr>
-          </thead>
 
-          <tbody>
-            {certificate.used_patterns.pressure_pattern.map((pattern) => (
-              <tr key={pattern.id}>
-                <td>{pattern.equipment}</td>
-                <td>{pattern.code}</td>
-                <td>{pattern.certificate}</td>
-                <td>{pattern.traceability}</td>
-                <td>{pattern.next_calibration}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
-
-      <section className="table-d-01__observations">
+      <section className="table-d-02__observations">
         <h2>Observaciones</h2>
 
         <div>
           <p>Patrón utilizado:</p>
-          <span>{certificate.observations}</span>
+          <span>{certificateData.description_pattern.join(', ')}</span>
         </div>
 
         <div>
           <p>
             Este certificado{' '}
-            {certificate.creditable ? 'es acreditado ✅' : 'no es acreditado'}
+            {certificateData.creditable
+              ? 'es acreditado ✅'
+              : 'no es acreditado'}
           </p>
         </div>
 
         <div>
           <p>Observaciones adicionales</p>
-          <span>{certificate.observations}</span>
+          <span>{certificateData.observations}</span>
         </div>
       </section>
     </div>
