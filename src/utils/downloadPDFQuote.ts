e7490a197f8d57692e84c47dae2ba71adbe3ee37
francisco-JGC <jjjchico1@gmail.com>
2024-07-03 08:30:36 -0600

    Refactor quote page to use handleGeneratePDFQuote utility function

---
+++ src/utils/downloadPDFQuote.ts
@@ -0,0 +1,32 @@
+import { toast } from 'sonner'
+import { fetchData } from './fetch'
+
+export const handleGeneratePDFQuote = async ({
+  id,
+  no,
+  company_name,
+}: {
+  id: number
+  no: string
+  company_name: string
+}) => {
+  toast.loading('Generando PDF...', {
+    description: 'Espere un momento por favor',
+  })
+  const response = await fetchData({
+    url: `quotes/request/pdf/${id}`,
+    method: 'GET',
+    responseType: 'blob',
+  })
+
+  if (response) {
+    const blob = new Blob([response], { type: 'application/pdf' })
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = `${no}_${company_name}.pdf`
+    link.click()
+    toast.success('PDF generado correctamente')
+  } else {
+    toast.error('Error al generar el PDF')
+  }
+}
