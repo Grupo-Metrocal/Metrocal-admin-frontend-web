89930db6d734f776a33f82ac6309f573209badc9
francisco-JGC <jjjchico1@gmail.com>
2024-08-09 15:27:13 -0600

    refactor: Update DescriptionPattern component to use select dropdown for pattern selection

--- src/app/dashboard/activities/view/update/methods/d_02/component/description_pattern.tsx
+++ src/app/dashboard/activities/view/update/methods/d_02/component/description_pattern.tsx
@@ -33,50 +33,69 @@ export const DescriptionPattern = ({
   }
   return (
     <div className="flex flex-col space-y-4">
-    <table className="w-full table-auto">
-      <thead>
-        <tr className="bg-gray-200">
-          <th className="border px-4 py-2">Patrón utilizado</th>
-        </tr>
-      </thead>
-      <tbody>
-        {patterns?.map((pattern, index) => (
-          <tr key={index}>
-            <td className="border px-4 py-2">
-              <input
-                className="w-full p-1 border rounded"
-                type="text"
-                value={pattern}
-                onChange={(e) => handlePatternChange(index, e.target.value)}
-                placeholder="Ingrese el patrón"
-              />
-            </td>
+      <table className="w-full table-auto">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border px-4 py-2">Patrón utilizado</th>
           </tr>
-        ))}
-        <tr>
+        </thead>
+        <tbody>
+          {patterns?.map((pattern, index) => (
+            <tr key={index}>
+              {/* <td className="border px-4 py-2">
+                <input
+                  className="w-full p-1 border rounded"
+                  type="text"
+                  value={pattern}
+                  onChange={(e) => handlePatternChange(index, e.target.value)}
+                  placeholder="Ingrese el patrón"
+                />
+              </td> */}
+
+              <div className="flex flex-col gap-[1em]">
+                <label htmlFor="unit" className="text-xs font-semibold ">
+                  Patrón utilizado
+                </label>
+                <select
+                  name="pattern"
+                  id="pattern"
+                  defaultValue={values.pattern}
+                  value={pattern}
+                  onChange={(e) => handlePatternChange(index, e.target.value)}
+                  placeholder="Ingrese el patrón"
+                  className="border border-gray-300 rounded-md p-2 h-fit"
+                >
+                  <option value="NI-MCPD-01">NI-MCPD-01</option>
+                  <option value="NI-MCPD-02">NI-MCPD-02</option>
+                  <option value="NI-MCPD-03">NI-MCPD-03</option>
+                </select>
+              </div>
+            </tr>
+          ))}
+          {/* <tr>
           <td className="border px-4 py-2">
             <button onClick={handleAddPattern}>Agregar patrón</button>
           </td>
-        </tr>
-      </tbody>
-    </table>
-    <div>
-      <AlertDialogModal
-        title="Guardar modificaciones"
-        description="¿Estás seguro de guardar las modificaciones?"
-        onConfirm={() =>
-          handleSaveInformation(
-            { ...values, descriptionPattern: patterns },
-            url,
-            false,
-          )
-        }
-        nameButton="Guardar modificaciones"
-        buttonStyle={{
-          margin: '1em 0',
-        }}
-      />
+        </tr> */}
+        </tbody>
+      </table>
+      <div>
+        <AlertDialogModal
+          title="Guardar modificaciones"
+          description="¿Estás seguro de guardar las modificaciones?"
+          onConfirm={() =>
+            handleSaveInformation(
+              { ...values, descriptionPattern: patterns },
+              url,
+              false,
+            )
+          }
+          nameButton="Guardar modificaciones"
+          buttonStyle={{
+            margin: '1em 0',
+          }}
+        />
+      </div>
     </div>
-  </div>
   )
 }
