50275a87cf723a3c014841852c17a3d6b8be03fb
francisco-JGC <jjjchico1@gmail.com>
2024-08-09 19:51:55 -0600

    refactor: Update ICertificate_D_02 interface to match changes in data structure

--- src/app/dashboard/certificates/interface/d-02.ts
+++ src/app/dashboard/certificates/interface/d-02.ts
@@ -27,68 +27,63 @@ export interface ICertificate_D_02 {
   pattern: string
   email: string
   equipment_information: EquipmentInformation
-  calibration_results: CalibrationResults
+  calibration_result: CalibrationResult
   environmental_conditions: EnvironmentalConditions
-  description_pattern: string[]
-  used_patterns: UsedPatterns
   creditable: boolean
+  descriptionPattern: DescriptionPattern[]
   observations: string
 }
 
 export interface EquipmentInformation {
   certification_code: string
   service_code: string
-  calibration_date: string
   certificate_issue_date: string
+  calibration_date: string
   object_calibrated: string
   maker: string
   serial_number: string
   model: string
   measurement_range: string
   resolution: string
-  identification_code: string
+  code: string
   applicant: string
   address: string
   calibration_location: string
 }
 
-export interface CalibrationResults {
-  calibration_accuracy_tests: CalibrationAccuracyTest[]
-}
-
-export interface CalibrationAccuracyTest {
-  C: number  
-  F: number 
-  K: number 
-  Q: number 
-  W: number 
+export interface CalibrationResult {
+  nominal_value: string[]
+  current_length: string[]
+  current_reading: string[]
+  deviation: string[]
+  uncertainty: [
+    string,
+    number,
+    number,
+    number,
+    string,
+    number,
+    number,
+    number,
+    string,
+    number,
+    string,
+  ]
 }
 
 export interface EnvironmentalConditions {
   temperature: string
   humidity: string
-  temperature2: string
-  humidity2: string
 }
 
 export interface DescriptionPattern {
-  id: number
-  pattern: string
-  observation: any
-  creditable: boolean
-}
-
-export interface UsedPatterns {
-  pressure_pattern: PressurePattern[]
-}
-
-export interface PressurePattern {
   id: number
   method: string
   equipment: string
   code: string
   certificate: string
   traceability: string
+  pattern_range: string
   next_calibration: string
   created_at: string
 }
