3f64eafa0ea3331a64612f413610fc3d1adf9680
francisco-JGC <jjjchico1@gmail.com>
2024-08-27 16:22:36 -0600

    refactor: Add quoteRecordsType interface for quote records

---
+++ src/types/quoteRecords.ts
@@ -0,0 +1,10 @@
+export interface quoteRecordsType {
+  id: number
+  client_company_name: string
+  quote_request_no: string
+  client_phone: string
+  quote_request_price: number
+  quote_request_created_at: string
+  approved_by: string
+  quote_request_status: string
+}
