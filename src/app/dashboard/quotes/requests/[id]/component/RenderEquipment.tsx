1f49b5e152b5e64e2ee6365cb5443c54b88e4ca6
francisco-JGC <jjjchico1@gmail.com>
2024-09-02 15:07:37 -0600

    feat: create service to  delete equipment

--- src/app/dashboard/quotes/requests/[id]/component/RenderEquipment.tsx
+++ src/app/dashboard/quotes/requests/[id]/component/RenderEquipment.tsx
@@ -4,6 +4,12 @@ import pencilIcon from '@/assets/icons/pencil.svg'
 import xMarkIcon from '@/assets/icons/xmark.svg'
 import Image from 'next/image'
 import type { IEquipmentQuoteRequest } from '../page'
+import { AlertDialogModal } from '@/components/AlertDialogModal'
+import { toast } from 'sonner'
+import { fetchData } from '@/utils/fetch'
+import { getCookie } from 'cookies-next'
+import { useAppDispatch, useAppSelector } from '@/redux/hook'
+import { handleDeleteEquipmentFromQuote } from '@/redux/features/quote/quoteSlice'
 
 interface IProps {
   equipment: IEquipmentQuoteRequest
@@ -13,21 +19,78 @@ interface IProps {
 }
 
 export const RenderEquipment = ({ equipment, onClick, selected }: IProps) => {
+  const quote = useAppSelector(state => state.quote)
+  const dispatch = useAppDispatch()
+
+  const handleDeleteEquipment = async () => {
+
+    if (quote.equipment.length === 1) {
+      return toast('la cotizacion necesita un servicio vigente')
+    }
+
+    toast.loading('Eliminando servicio')
+
+    const response = await fetchData({
+      url: 'quotes/equipment/delete-from-quote',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getCookie('token')}`,
+      },
+      body: {
+        equipmentID: equipment.id,
+        quoteID: quote.id
+      }
+    })
+
+    toast.dismiss()
+
+    if (response.success) {
+      toast.success('Servicio eliminado de la cotización')
+      dispatch(handleDeleteEquipmentFromQuote(equipment.id))
+    } else {
+      toast.error('Hubo un error al eliminar este servicio', {
+        description: 'Porfavor vuelva a intentarlo'
+      })
+    }
+
+  }
+
   return (
     <div
       className={`equipment 
       ${selected && 'equipment--selected'}
-      ${
-        equipment.status === 'rejected'
+      ${equipment.status === 'rejected'
           ? 'equipment--rejected'
           : equipment.status === 'done'
-          ? 'equipment--reviewed'
-          : ''
-      }
+            ? 'equipment--reviewed'
+            : ''
+        }
       `}
       onClick={onClick}
       data-equipment-status={equipment.status}
     >
+
+      <AlertDialogModal
+        nameButton='X'
+        onConfirm={handleDeleteEquipment}
+        title='Eliminar servicio de la cotizacion'
+        description='Estas seguro de eliminar este servicio de la cotización?, no podras recuperar la informacion eliminada'
+        nameButtonConfirm='Eliminar'
+        buttonStyle={{
+          boxShadow: 'none',
+          width: '20px',
+          height: '20px',
+          position: 'absolute',
+          top: 0,
+          background: 'tomato',
+          borderRadius: '3px 10px 3px 3px',
+          border: '3px solid tomato',
+          boxSizing: 'content-box',
+          right: 0,
+          padding: 0
+        }}
+      />
       <div className="status">
         <div className="img">
           <Image
@@ -35,8 +98,8 @@ export const RenderEquipment = ({ equipment, onClick, selected }: IProps) => {
               equipment.status === 'done'
                 ? checkMarkIcon
                 : equipment.status === 'rejected'
-                ? xMarkIcon
-                : pencilIcon
+                  ? xMarkIcon
+                  : pencilIcon
             }
             alt="status"
           />
