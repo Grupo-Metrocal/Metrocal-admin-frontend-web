b42f76599c26c6874be0cf25074bee3608a1c95f
francisco-JGC <jjjchico1@gmail.com>
2024-09-05 13:30:02 -0600

    feat: create item quote modified component

---
+++ src/app/dashboard/quotes/view/[id]/_components/registerQuoteItem.tsx
@@ -0,0 +1,40 @@
+import { formatQuoteCode } from "@/utils/formatQuoteCode";
+import { IQuote } from "../../../requests/[id]/page";
+import { formatDate } from "@/utils/formatDate";
+import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import messageIcon from '@/assets/icons/comment.svg'
+import Image from "next/image";
+
+interface IProps {
+  quote: IQuote
+  index: number
+}
+
+export const RegisterQuoteItem = ({ quote, index }: IProps) => {
+  return (
+    <div className="shadow-lg w-60 p-4 rounded flex flex-col gap-2 relative">
+      <div className="flex justify-between gap-2 items-center">
+        <h3 className="text-base">{quote.no}</h3>
+        <Popover>
+          <PopoverTrigger>
+            <Image src={messageIcon} alt="Icono Mensaje de modificación" width={20} />
+          </PopoverTrigger>
+          <PopoverContent className="bg-white p-4 shadow-lg w-full z-20">
+            <p className="w-96">{quote.quote_modification_message || 'No hay mensjae'}</p>
+          </PopoverContent>
+        </Popover>
+      </div >
+
+
+      <div className="flex justify-between gap-2 items-center">
+        <span className="text-gray-400">{formatDate(quote.updated_at)}</span>
+
+        <Button variant={"outline"}>
+          <Download width={17} />
+        </Button>
+      </div>
+    </div >
+  )
+}
