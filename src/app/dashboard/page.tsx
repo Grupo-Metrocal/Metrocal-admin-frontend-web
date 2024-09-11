447930bf357590986a6aee3e32500cb84fc1edba
francisco-JGC <jjjchico1@gmail.com>
2024-09-11 13:35:34 -0600

    feat: render Quotation Details

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { LayoutPage } from '@/components/LayoutPage'
-import { StatisticsActivity } from './certificates/records/components/statistics'
 import { QuoteFluctuationStatistic } from './_component/quoteFluctuationStatistic'
+import { QuotationsDetails } from './_component/quotationDetails'
 export default function Page() {
   return (
-    <LayoutPage title="Panel de control">
-      <StatisticsActivity />
-      <QuoteFluctuationStatistic />
+    <LayoutPage title="Panel de control"
+    >
+      <div className='flex flex-col gap-4'>
+        <QuotationsDetails />
+        <QuoteFluctuationStatistic />
+      </div>
     </LayoutPage>
   )
 }
