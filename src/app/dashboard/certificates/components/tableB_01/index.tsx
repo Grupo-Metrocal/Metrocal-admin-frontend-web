b9c5d6f0521602fe0bd41a9888aefeaee92a66a4
francisco-JGC <jjjchico1@gmail.com>
2024-10-29 23:38:48 -0600

    fix

--- src/app/dashboard/certificates/components/tableB_01/index.tsx
+++ src/app/dashboard/certificates/components/tableB_01/index.tsx
@@ -11,6 +11,9 @@ export const TableB_01 = ({
   id: number
   method_name: string
 }) => {
+
+  const totalRows = certificate?.calibration_results?.result_test?.reference_mass?.length || 0;
+
   return (
     <div className="table-b-01">
       <section className="table-b-01__equipment-information">
@@ -83,78 +86,155 @@ export const TableB_01 = ({
       </section>
       <section className="table-b-01__calibration-result">
         <h2>Tabla de resultados de la calibración</h2>
-        <table>
+        <table className="w-full border-collapse">
           <thead>
             <tr>
-              <th>Masa de referencia</th>
-              <th>Indicación del equipo</th>
-              <th>Error</th>
-              <th>Repetibilidad</th>
-              <th>Excentricidad maxima</th>
-              <th>Incertidumbre expandida</th>
+              <th className="border py-2 px-4">Masa de referencia</th>
+              <th className="border py-2 px-4">Indicación del equipo</th>
+              <th className="border py-2 px-4">Error</th>
+              <th className="border py-2 px-4">Repetibilidad</th>
+              <th className="border py-2 px-4">Excentricidad máxima</th>
+              <th className="border py-2 px-4">Incertidumbre expandida k=2</th>
             </tr>
           </thead>
           <tbody>
+            {
+              <tr className="text-center">
+                <td className="border py-2 px-4">
+                  {renderValue(certificate?.calibration_results?.result_test?.reference_mass[0])}
+                </td>
+                <td className="border py-2 px-4">
+                  {renderValue(certificate.calibration_results.result_test.equipment_indication[0])}
+                </td>
+                <td className="border py-2 px-4">
+                  {renderValue(certificate.calibration_results.result_test.error[0])}
+                </td>
+                {
+                  <td className="border py-2 px-4">
+                    {renderValue(certificate.calibration_results.result_test.error[0])}
+                  </td>
+                }
+                {
+                  <td className="border py-2 px-4">
+                    {renderValue(certificate.calibration_results.result_test.repeatability[0])}
+                  </td>
+                }
+                <td className="border py-2 px-4">
+                  {renderValue(certificate.calibration_results.result_test.uncertainty[0])}
+                </td>
+              </tr>
+            }
             {certificate?.calibration_results?.result_test?.reference_mass?.map(
-              (result, index) => (
-                <tr key={index}>
-                  <td>{renderValue(result)} </td>
-                  <td>
+              (_result, index) => (
+                index > 0 && <tr key={index} className="text-center">
+                  <td className="border py-2 px-4">
+                    {renderValue(certificate?.calibration_results?.result_test?.reference_mass?.[index])}
+                  </td>
+                  <td className="border py-2 px-4">
                     {renderValue(certificate.calibration_results.result_test.equipment_indication[index])}
                   </td>
-                  <td>
+                  <td className="border py-2 px-4">
                     {renderValue(certificate.calibration_results.result_test.error[index])}
                   </td>
-                  <td>
-                    {renderValue(certificate.calibration_results.result_test.repeatability[index])}
-                  </td>
-                  <td>
-                    {renderValue(certificate.calibration_results.result_test.maximum_eccentricity[index])}
-                  </td>
-                  <td>
+                  {index === 0 || index === 1 && (
+                    <td className="border py-2 px-4" rowSpan={index === 1 ? totalRows : 0}>
+                      {renderValue(certificate.calibration_results.result_test.repeatability[index])}
+                    </td>
+                  )}
+                  {index === 0 || index === 1 && (
+                    <td className="border py-2 px-4" rowSpan={index === 1 ? totalRows : 0}>
+                      {renderValue(certificate.calibration_results.result_test.maximum_eccentricity[index])}
+                    </td>
+                  )}
+                  <td className="border py-2 px-4">
                     {renderValue(certificate.calibration_results.result_test.uncertainty[index])}
                   </td>
                 </tr>
-              ),
+              )
             )}
           </tbody>
         </table>
-      </section>
-
-      {certificate.show_additional_table !== '' && (
-        <section className="table-b-01__calibration-result">
-          <h2>Tabla de resultados de la calibración</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Masa de referencia</th>
-                <th>Indicación del equipo</th>
-                <th>Error</th>
-                <th>Repetibilidad</th>
-                <th>Excentricidad maxima</th>
-                <th>Incertidumbre expandida</th>
-              </tr>
-            </thead>
-            <tbody>
-              {certificate?.calibration_results?.result_test_extra?.reference_mass?.map(
-                (item, index) => (
-                  <tr key={index}>
-                    <td>{renderValue(item)}</td>
-                    <td>{renderValue(certificate.calibration_results.result_test_extra.equipment_indication[index])}</td>
-                    <td>{renderValue(certificate.calibration_results.result_test_extra.error[index])}</td>
-                    <td>{renderValue(certificate.calibration_results.result_test_extra.repeatability[index])}</td>
-                    <td>{renderValue(certificate.calibration_results.result_test_extra.maximum_eccentricity[index])}</td>
-                    <td>{renderValue(certificate.calibration_results.result_test_extra.uncertainty[index])}</td>
+      </section >
+      <br />
+      <br />
+      {
+        certificate.show_additional_table !== '' && (
+          <section className="table-b-01__calibration-result">
+            <h2>Tabla de resultados de la calibración</h2>
+            <table className="w-full border-collapse">
+              <thead>
+                <tr>
+                  <th className="border py-2 px-4">Masa de referencia</th>
+                  <th className="border py-2 px-4">Indicación del equipo</th>
+                  <th className="border py-2 px-4">Error</th>
+                  <th className="border py-2 px-4">Repetibilidad</th>
+                  <th className="border py-2 px-4">Excentricidad máxima</th>
+                  <th className="border py-2 px-4">Incertidumbre expandida k=2</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  <tr className="text-center">
+                    <td className="border py-2 px-4">
+                      {renderValue(certificate?.calibration_results.result_test_extra?.reference_mass[0])}
+                    </td>
+                    <td className="border py-2 px-4">
+                      {renderValue(certificate.calibration_results.result_test_extra.equipment_indication[0])}
+                    </td>
+                    <td className="border py-2 px-4">
+                      {renderValue(certificate.calibration_results.result_test_extra.error[0])}
+                    </td>
+                    {
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate.calibration_results.result_test_extra.error[0])}
+                      </td>
+                    }
+                    {
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate.calibration_results.result_test_extra.repeatability[0])}
+                      </td>
+                    }
+                    <td className="border py-2 px-4">
+                      {renderValue(certificate.calibration_results.result_test_extra.uncertainty[0])}
+                    </td>
                   </tr>
-                ),
-              )}
-            </tbody>
-          </table>
-        </section>
-      )
+                }
+                {certificate?.calibration_results?.result_test_extra?.reference_mass?.map(
+                  (_result, index) => (
+                    index > 0 && <tr key={index} className="text-center">
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate?.calibration_results?.result_test_extra?.reference_mass?.[index])}
+                      </td>
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate.calibration_results.result_test_extra.equipment_indication[index])}
+                      </td>
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate.calibration_results.result_test_extra.error[index])}
+                      </td>
+                      {index === 0 || index === 1 && (
+                        <td className="border py-2 px-4" rowSpan={index === 1 ? totalRows : 0}>
+                          {renderValue(certificate.calibration_results.result_test_extra.repeatability[index])}
+                        </td>
+                      )}
+                      {index === 0 || index === 1 && (
+                        <td className="border py-2 px-4" rowSpan={index === 1 ? totalRows : 0}>
+                          {renderValue(certificate.calibration_results.result_test_extra.maximum_eccentricity[index])}
+                        </td>
+                      )}
+                      <td className="border py-2 px-4">
+                        {renderValue(certificate.calibration_results.result_test_extra.uncertainty[index])}
+                      </td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </section>
+        )
       }
 
-      <section className="table-b-01__environmental-conditions">
+      <section className="table-p-01__calibration-result">
+        <br />
         <h2>Condiciones ambientales</h2>
 
         <table>
@@ -171,7 +251,7 @@ export const TableB_01 = ({
             </tr>
           </tbody>
         </table>
-      </section>
+      </section >
 
       <section className="table-p-01__calibration-result">
         <h2>Descripción de patrones utilizados</h2>
