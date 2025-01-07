e03becb2c03e844006260bbcec54efa1bc9b866a
francisco-JGC <jjjchico1@gmail.com>
2025-01-07 00:44:26 -0600

    fix

--- src/app/dashboard/activities/view/[id]/component/t_05/calibration-result.tsx
+++ src/app/dashboard/activities/view/[id]/component/t_05/calibration-result.tsx
@@ -18,71 +18,74 @@ export const CalibrationResults = ({
   return (
     <div>
       <div className="flex flex-col space-y-4 border p-4">
-        <table className="min-w-full border-collapse border border-gray-400">
-          <thead>
-            <tr>
-              <th
-                rowSpan={2}
-                className="border border-gray-400 p-2 bg-gray-200"
-              >
+        <div className="min-w-full p-4 bg-gray-50 rounded-lg">
+          <header className="flex gap-4 mb-4">
+            <section className="flex items-center justify-center border border-gray-300 bg-white p-3 w-[150px] rounded-md shadow-sm">
+              <span className="text-center font-semibold text-gray-800">
                 Temperatura
-              </th>
+              </span>
+            </section>
 
-              {calibration_results?.results[0]?.calibrations?.map((result) => {
-                return (
-                  <th
-                    colSpan={2}
-                    className="border border-gray-400 p-2 bg-gray-200"
-                    key={result.point_number}
-                  >
-                    Punto{' '}
-                    {result.point_number === -1
-                      ? '1 Final'
-                      : result.point_number}
-                  </th>
-                )
-              })}
-            </tr>
-            <tr>
-              {calibration_results?.results[0]?.calibrations?.map((result) => {
-                return (
-                  <>
-                    <th className="border border-gray-400 p-2 bg-gray-200">
-                      Inicial
-                    </th>
-                    <th className="border border-gray-400 p-2 bg-gray-200">
-                      Final
-                    </th>
-                  </>
-                )
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {calibration_results?.results?.map((result) => {
-              return (
-                <tr key={result?.temperature}>
-                  <td className="border border-gray-400 p-2 text-center">
-                    {result.temperature}
-                  </td>
+            <section className="flex gap-6 w-full overflow-x-auto">
+              {calibration_results.results.map((result) => (
+                <div
+                  className="flex flex-col w-full max-w-[140px] p-2 border border-gray-300 bg-white rounded-md shadow-sm"
+                  key={result?.point_number}
+                >
+                  <span className="text-center font-semibold text-gray-700 mb-2">
+                    Punto {result?.point_number === -1 ? '1 final' : result?.point_number}
+                  </span>
+                  <div className="flex justify-between items-center gap-2">
+                    <span className="border border-gray-300 w-full text-center py-1 bg-gray-100 rounded-md">
+                      Lp
+                    </span>
+                    <span className="border border-gray-300 w-full text-center py-1 bg-gray-100 rounded-md">
+                      Libc
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </section>
+          </header>
 
-                  {result?.calibrations.map((calibration) => {
-                    return (
-                      <>
-                        <td className="border border-gray-400 p-2 text-center">
-                          {calibration?.initial}
-                        </td>
-                        <td className="border border-gray-400 p-2 text-center">
-                          {calibration?.final}
-                        </td>
-                      </>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+          <section className="flex w-full gap-4">
+            <div className="flex flex-col w-[150px] gap-2">
+              {calibration_results.results.map((result) => (
+                <span
+                  className="p-2 text-center w-full border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-300"
+                  key={result?.point_number}
+                >{result.temperature}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex w-full gap-6 overflow-x-auto">
+              {calibration_results.results.map((result) => (
+                <div
+                  className="flex flex-col w-full max-w-[140px] gap-2"
+                  key={result?.point_number}
+                >
+                  {result.calibrations.map((calibration, index) => (
+                    <div
+                      className="w-full flex justify-between gap-2"
+                      key={index}
+                    >
+                      <span
+                        className="border border-gray-300 w-full text-center p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-300"
+
+                      >{calibration.initial}</span>
+                      <span
+                        className="border border-gray-300 w-full text-center p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-300"
+                      >
+                        {calibration.final}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
       </div>
 
       <ReportMethodActivity
